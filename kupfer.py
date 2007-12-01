11872eb6daec4115549cc7e935c197e973dd90c5
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2007-12-01 02:08:49 +0100

    Create class KupferSearch: Objectify the search

--- kupfer.py
+++ kupfer.py
@@ -1,95 +1,100 @@
 #!/usr/bin/python
 
-def rank_string(s, key):
-	s = s.lower()
-	key = key.lower()
-	rank = 0
-	if s.startswith(key):
-		rank+=10
-	elif key in s:
-		rank+=5
-	if key in s.split(" ./"):
-		# exact word match
-		rank+=8
-	return rank
+class KupferSearch (object):
+	
+	def __init__(self, wordsep=" .-_"):
+		self.wordsep = wordsep
 
-def split_at(s, seps):
-	"""
-	Split at string at any char in seps
-	"""
-	parts = []
-	last = 0
-	for i, c in enumerate(s):
-		if c in seps:
-			parts.append(s[last:i])
-			last = i+1
-	if last == 0:
-		parts.append(s)
-	else:
-		parts.append(s[last:])
-	return parts
-
-def common_letters(s, key):
-	"""
-	count number of common letters
-	(in order)
-	"""
-	idx = 0
-	for c in s:
-		if c == key[idx]:
-			idx += 1
-			if idx == len(key):
-				break
-	return idx
-
-def abbrev_str(s):
-	words = split_at(s," .-_")
-	first_chars = "".join([w[0] for w in words if len(w)])
-	return first_chars
-
-def upper_str(s):
-	return "".join([c for c in s if c.isupper()])
-
-def rank_objects(objects, key):
-	"""
-	objects --
-	key -- 
-	"""
-	normal_w = 10
-	abbrev_w = 7 
-	common_letter_w = 3
-	part_w = 1
-	rank_list = []
-
-	def rank_key(obj, key):
+	def rank_string(self, s, key):
+		s = s.lower()
+		key = key.lower()
 		rank = 0
-		rank += normal_w* rank_string(i, key)
-		abbrev = abbrev_str(i)
-		rank += abbrev_w * rank_string(abbrev, key)
-		rank += common_letter_w * common_letters(i, key)
-
+		if s.startswith(key):
+			rank+=10
+		elif key in s:
+			rank+=5
+		if key in self.split_at(s, self.wordsep):
+			# exact word match
+			rank+=8
 		return rank
 
-	for i in objects:
-		rank = 0
-		rank += normal_w*rank_key(i, key)
-		# do parts
-		keyparts = key.split()
-		for part in keyparts:
-			rank += part_w*rank_key(i, part)
-		
-		rank_list.append((rank,i))
-	rank_list.sort(key= lambda item: item[0], reverse=True)
-	return rank_list
+	def split_at(self, s, seps):
+		"""
+		Split at string at any char in seps
+		"""
+		parts = []
+		last = 0
+		for i, c in enumerate(s):
+			if c in seps:
+				parts.append(s[last:i])
+				last = i+1
+		if last == 0:
+			parts.append(s)
+		else:
+			parts.append(s[last:])
+		return parts
 
-def search_objects(objects, key):
-	"""
-	
-	objects -- list of objects
-	key -- string key
-	"""
-	ranked_str = rank_objects(objects, key)
-	return ranked_str
+	def common_letters(self, s, key):
+		"""
+		count number of common letters
+		(in order)
+		"""
+		idx = 0
+		for c in s:
+			if c == key[idx]:
+				idx += 1
+				if idx == len(key):
+					break
+		return idx
+
+	def abbrev_str(self, s):
+		words = self.split_at(s, self.wordsep)
+		first_chars = "".join([w[0] for w in words if len(w)])
+		return first_chars
+
+	def upper_str(self, s):
+		return "".join([c for c in s if c.isupper()])
+
+	def rank_objects(self, objects, key):
+		"""
+		objects --
+		key -- 
+		"""
+		normal_w = 10
+		abbrev_w = 7 
+		common_letter_w = 3
+		part_w = 1
+		rank_list = []
+
+		def rank_key(obj, key):
+			rank = 0
+			rank += normal_w* self.rank_string(i, key)
+			abbrev = self.abbrev_str(i)
+			rank += abbrev_w * self.rank_string(abbrev, key)
+			rank += common_letter_w * self.common_letters(i, key)
+
+			return rank
+
+		for i in objects:
+			rank = 0
+			rank += normal_w * rank_key(i, key)
+			# do parts
+			keyparts = key.split()
+			for part in keyparts:
+				rank += part_w * rank_key(i, part)
+			
+			rank_list.append((rank,i))
+		rank_list.sort(key= lambda item: item[0], reverse=True)
+		return rank_list
+
+	def search_objects(self, objects, key):
+		"""
+		
+		objects -- list of objects
+		key -- string key
+		"""
+		ranked_str = self.rank_objects(objects, key)
+		return ranked_str
 
 import gtk 
 class WindowControl (object):
@@ -137,12 +142,14 @@ if __name__ == '__main__':
 	# get items in curdir
 	path.walk("/home/ulrik/Desktop", get_listing, dirlist)
 
+	kupfer = KupferSearch()
+
 	def do_search(text, context=None):
 		# print "Type in search string"
 		# in_str = raw_input()
 		if not len(text):
 			return
-		ranked_str = search_objects(dirlist, text)
+		ranked_str = kupfer.search_objects(dirlist, text)
 
 		for idx, s in enumerate(ranked_str):
 			print s
