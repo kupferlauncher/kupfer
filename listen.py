50a856156e0c4dc1ae5bb2a2c7860acfb7187a83
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2008-04-02 16:38:29 +0200

    Proceed without dbus if it is unavailable
    If there is an error in finding dbus.Bus() we catch the exception and
    print an error message. Dbus registration of keyboard shortcuts are
    skipped without further error messages.

--- listen.py
+++ listen.py
@@ -5,12 +5,20 @@ This module is a singelton, that sets up callbacks to dbus signals
 import dbus
 import dbus.glib
 
-session_bus = dbus.Bus()
+# if dbus unavailable print the exception here
+# but further actions (register) will fail without warning
+try:
+	session_bus = dbus.Bus()
+except dbus.exceptions.DBusException, exc:
+	session_bus = None
+	print exc
+
 interface_name = "org.gnome.Kupfer"
 
 def register(signal_name, callback):
 	"""
 	Register function `callback` for `signal_name`
 	"""
-	session_bus.add_signal_receiver(callback, signal_name, interface_name, bus_name=None, path=None)
+	if session_bus:
+		session_bus.add_signal_receiver(callback, signal_name, interface_name, bus_name=None, path=None)
 
