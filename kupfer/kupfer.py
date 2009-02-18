43f64870f3c2c3c9d4af99c392a3d9500828526b
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-02-18 04:02:45 +0100

    Relevance: Remove obsolete ranking bits

--- kupfer/kupfer.py
+++ kupfer/kupfer.py
@@ -61,80 +61,10 @@ class Search (object):
 			value = "".join(c for c in val if c not in self.ignorechars)
 
 			rankable = Rankable(value, obj)
-			rankable._words = tuple(split_at(value, self.wordsep))
-			rankable._abbrev = "".join(w[:1].lower() for w in rankable._words)
+			#rankable._words = tuple(split_at(value, self.wordsep))
+			#rankable._abbrev = "".join(w[:1].lower() for w in rankable._words)
 			all.append(rankable)
 		return all
-
-	def rank_string(self, s, key):
-		# match values
-		exact_v = 20
-		start_v = 10
-		substr_v = 5
-
-		rank = 0
-		if s == key:
-			rank += exact_v
-		elif s.startswith(key):
-			rank += start_v
-		elif key in s:
-			rank += substr_v
-		else:
-			rank += self.common_prefix(s, key)
-		return rank
-	
-	def rank_words(self, item, key):
-		# match weights
-		word_w = 2
-		rank = 0
-		if key in item._words:
-			# exact subword match
-			rank += word_w*len(key)
-		else:
-			# score prefixes of subwords
-			# example:
-			# key: contdesk
-			# matches configure-the-thing.desktop as 3-1-0-4 for a score of 7
-			# key: conddesk matches only 3-0-0-1
-			idx = 0
-			word_pfx = 0
-			for w in item._words:
-				pfx = self.common_prefix(w, key[idx:])
-				idx += pfx
-				word_pfx += pfx
-			rank += word_pfx
-
-		return rank
-
-	def common_letters(self, s, key, lower=False):
-		"""
-		count number of common letters
-		(in order)
-
-		if lower is True, will lowercase
-		"""
-		if lower:
-			s = s.lower()
-			key = key.lower()
-		idx = 0
-		for c in s:
-			if idx == len(key):
-				break
-			if c == key[idx]:
-				idx += 1
-		return idx
-
-	def common_prefix(self, s, key):
-		"""
-		count nbr of common letters in common prefix
-		"""
-		common = 0
-		for c1, c2 in zip(s, key):
-			if c1 == c2:
-				common += 1
-			else:
-				break
-		return common
 
 	def rank_objects(self, objects, key):
 		"""
@@ -144,43 +74,6 @@ class Search (object):
 		from relevance import score
 		for obj in objects:
 			obj.rank = score(obj.value, key)*100
-		return
-
-		normal_w = 10
-		abbrev_w = 5
-		words_w = 10
-		common_letter_w = 3
-		all_common_w = 9
-		part_w = 1
-
-		lower_key = key.lower()
-
-		def rank_key(val, abbrev, key):
-			rank = 0
-			rank += normal_w * self.rank_string(val, key)
-			rank += abbrev_w * self.rank_string(abbrev, key)
-
-			com_let = self.common_letters(val, key)
-			if com_let == len(key): rank += all_common_w * com_let
-			else: rank += common_letter_w * com_let
-			rank += common_letter_w * self.common_letters(abbrev, key)
-			return rank
-
-		for item in objects:
-			val = item.value.lower()
-			abbrev = item._abbrev
-			rank = 0
-			rank += normal_w * rank_key(val, abbrev, lower_key)
-			rank += normal_w * words_w * self.rank_words(item, lower_key)
-			# do parts
-			keyparts = tuple(split_at(lower_key, self.wordsep))
-			if len(keyparts) > 1:
-				for part in keyparts:
-					if not part:
-						continue
-					rank += part_w * rank_key(val, abbrev, part)
-					rank += part_w * words_w * self.rank_words(item, part)
-			item.rank = rank
 
 	def search_objects(self, key):
 		"""
@@ -194,26 +87,12 @@ class Search (object):
 			self.old_list = None
 		else:
 			search_base = self.old_list
-		# only sort on worthwhile objects
-		# only filter on first word to make it simple
-		for key_part in split_at(key, self.wordsep):
-			if key_part:
-				break
-		else:
-			key_part = key
-		part_len = len(key_part)
-		for item in search_base:
-			# item has to contain all of key, in the right order
-			com = self.common_letters(item.value, key_part, lower=True)
-			item.rank = (part_len == com)
-
-		objects = [item for item in search_base if item.rank]
 
 		self.old_key = key
-		self.old_list = objects
+		self.old_list = search_base
 
 		# do the searching
-		self.rank_objects(objects, key)
-		objects.sort(key=lambda item: (-item.rank, item.value), reverse=False)
-		return objects
+		self.rank_objects(search_base, key)
+		search_base.sort(key=lambda item: (-item.rank, item.value), reverse=False)
+		return search_base
 	
