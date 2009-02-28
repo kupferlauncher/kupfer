dfeb92c450e911daac5db7bde4e2a9f1116a8f38
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-02-28 21:35:39 +0100

    Screen plugin: Add descriptions, sessions say Attached/Detached etc

--- kupfer/extensions/screen.py
+++ kupfer/extensions/screen.py
@@ -1,10 +1,35 @@
-
 from kupfer.objects import Leaf, Action, Source
+
+def screen_sessions_infos():
+	"""
+	Yield tuples of pid, name, time, status
+	for running screen sessions
+	"""
+	from os import popen
+	pipe = popen("screen -list")
+	output = pipe.read()
+	for line in output.splitlines():
+		fields = line.split("\t")
+		if len(fields) == 4:
+			empty, pidname, time, status = fields
+			pid, name = pidname.split(".", 1)
+			time = time.strip("()")
+			status = status.strip("()")
+			yield (pid, name, time, status)
 
 class ScreenSession (Leaf):
 	"""Represented object is the session pid as string"""
 	def get_actions(self):
 		return (AttachScreen(),)
+
+	def get_description(self):
+		for pid, name, time, status in screen_sessions_infos():
+			if self.object == pid:
+				break
+		else:
+			return "%s (%s)" % (self.name, self.object)
+		return "%s session %s (%s) created %s" % (status, name, pid, time)
+
 	def get_icon_name(self):
 		return "gnome-window-manager"
 
@@ -15,23 +40,16 @@ class ScreenSessionsSource (Source):
 	def is_dynamic(self):
 		return True
 	def get_items(self):
-		from os import popen
-		pipe = popen("screen -list")
-		output = pipe.read()
-		sessions = []
-		for line in output.splitlines():
-			fields = line.split("\t")
-			if len(fields) == 4:
-				empty, pidname, time, status = fields
-				pid, name = pidname.split(".", 1)
-				sessions.append(ScreenSession(pid, name))
-		return sessions
+		for pid, name, time, status in screen_sessions_infos():
+			yield ScreenSession(pid, name)
+
+	def get_description(self):
+		return "Active GNU Screen sessions"
 	def get_icon_name(self):
 		return "terminal"
 
 class AttachScreen (Action):
 	"""
-	Execute executable file (FileLeaf)
 	"""
 	def __init__(self):
 		name = "Attach"
