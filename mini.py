0cdd51471b8e4944b8e1e23b5c8b153e4993860a
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-03-01 03:27:07 +0100

    mini: Update mini test script to newest state

--- mini.py
+++ mini.py
@@ -11,11 +11,13 @@ class Test (object):
 		dc.connect("search-result", self.got_result)
 		dc.connect("predicate-result", self.got_predicates)
 		dc.connect("launched-action", self.cont)
+		dc.connect("new-source", self.cont)
 		self.Leaf = None
 		self.Action = None
 		self.dc = dc
+		self.waiting = []
 
-	def cont(self, sender, leaf, action):
+	def cont(self, *args, **kwargs):
 		gobject.idle_add(self.loop)
 
 	def got_predicates(self, sender, result, matchview, ctx):
@@ -30,29 +32,46 @@ class Test (object):
 			print m
 		obj = result and result.object or None
 		self.Leaf = obj
-		print obj
 		gobject.idle_add(self.loop)
 
+	def wait(self, context=None):
+		ret = gobject.timeout_add_seconds(2, self._waited, context)
+		self.waiting.append(ret)
+
+	def _waited(self, context=None):
+		if self.waiting:
+			print "No response"
+			self.loop()
+
 	def loop(self):
+		# clear waiting state
+		for ret in self.waiting:
+			gobject.source_remove(ret)
+		del self.waiting[:]
+
+		if self.Leaf:
+			print "%s -- %s" % (self.Leaf, self.Action)
+			print '"%s"' % self.Leaf.get_description()
 		try:
 			key = raw_input("kupfer> ")
 		except EOFError:
 			raise SystemExit
-		key = key.lower()
-		parts = key.split(" ", 1)
-		if self.Leaf and parts[0] == "a":
-			if len(parts) > 1:
-				key = parts[1]
-			else:
-				key = None
-			dc.search_predicate(self.Leaf, key)
-		elif key == "x" and self.Action:
+		key = key.lower().strip()
+		if key == "//" and self.Action:
 			gobject.idle_add(self.dc.eval_action, self.Leaf, self.Action)
+		elif self.Leaf and key and key[0] == "/":
+			key = key[1:]
+			if key == "/": key = ""
+			dc.search_predicate(self.Leaf, key)
+		elif key == "<":
+			dc.browse_up()
+		elif key == ">" and self.Leaf:
+			dc.browse_down(self.Leaf)
 		else:
 			self.Leaf = None
 			self.Action = None
 			dc.search(key)
-		print self.Leaf, self.Action
+		self.wait()
 
 def waiting(msg):
 	print msg
@@ -64,11 +83,8 @@ if __name__ == '__main__':
 	srcs.append(objects.FileSource(["/home/ulrik",], depth=1))
 	srcs.append(objects.AppSource())
 	srcs.append(extensions.screen.ScreenSessionsSource())
-	if len(srcs) == 1:
-		src = srcs[0]
-	else:
-		src = objects.MultiSource(srcs)
-	dc = data.DataController(src)
+	dc = data.DataController()
+	dc.set_sources(srcs, [])
 	print dc
 	gobject.threads_init()
 	gobject.set_application_name("kupfer-mini")
