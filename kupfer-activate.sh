3804f84604028a93f59eb2b449b7a10431b57310
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-03-04 22:52:17 +0100

    kupfer-activate.sh: Greatest hack ever
    We can enable Startup Notification in kupfer.desktop. Normally, the
    notification would continue to just spin if we call kupfer over dbus,
    but now we can invoke gtk.gdk method to stop the startup notification,
    right from the kupfer-activate.sh script!

--- kupfer-activate.sh
+++ kupfer-activate.sh
@@ -3,4 +3,4 @@
 # Try to spawn kupfer via dbus, else go to python
 dbus-send --print-reply --dest=se.kaizer.kupfer /interface se.kaizer.kupfer.Listener.ShowHide >/dev/null 2>&1
 
-test $? != 0 && python -m kupfer $*
+test $? != 0 && python -m kupfer $* || python -c "import gtk.gdk; gtk.gdk.notify_startup_complete()"
