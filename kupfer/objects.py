f121211a6da540be76172a2b9c9af768b8466c77
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-03-07 14:35:53 +0100

    FileLeaf: Use "gtk-file" as fallback icon

--- kupfer/objects.py
+++ kupfer/objects.py
@@ -194,6 +194,8 @@ class FileLeaf (Leaf):
 	def get_pixbuf(self):
 		uri = gnomevfs.get_uri_from_local_path(self.object)
 		icon = icons.get_icon_for_uri(uri, self.icon_size)
+		if not icon:
+			icon = icons.get_icon_for_name("gtk-file", self.icon_size)
 		return icon
 
 def ContstructFileLeaf(obj, name):
