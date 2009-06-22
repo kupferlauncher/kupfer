4ef7affe5b3c508b58970705c5f1599b41abb10a
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-06-23 00:07:56 +0200

    plugin.applications: Move AppSource to plugin

--- kupfer/objects.py
+++ kupfer/objects.py
@@ -797,40 +797,6 @@ class MultiSource (Source):
 	def get_icon_name(self):
 		return "folder-saved-search"
 
-class AppSource (Source):
-	"""
-	Applications source
-
-	This Source contains all user-visible applications (as given by
-	the desktop files)
-	"""
-	def __init__(self):
-		super(AppSource, self).__init__(_("All Applications"))
-	
-	def get_items(self):
-		from gio import app_info_get_all
-		# Choosing only item.should_show() items misses all Preference applets
-		# so we use a slight heurestic
-		taken = set()
-		for item in app_info_get_all():
-			if item.should_show():
-				yield AppLeaf(item)
-				taken.add(item.get_executable())
-		# Re-run and take some more
-		for item in app_info_get_all():
-			if (not item.should_show() and item.get_executable() not in taken
-					and (not item.supports_files() and not
-						item.supports_uris())):
-				yield AppLeaf(item)
-				taken.add(item.get_executable())
-
-	def get_description(self):
-		return _("All applications and preferences")
-
-	def get_icon_name(self):
-		return "gnome-applications"
-
-
 class UrlLeaf (Leaf):
 	def __init__(self, obj, name):
 		super(UrlLeaf, self).__init__(obj, name)
---
+++ kupfer/plugin/applications.py
@@ -0,0 +1,37 @@
+from kupfer.objects import Source, AppLeaf
+
+__kupfer_sources__ = ("AppSource", )
+
+class AppSource (Source):
+	"""
+	Applications source
+
+	This Source contains all user-visible applications (as given by
+	the desktop files)
+	"""
+	def __init__(self):
+		super(AppSource, self).__init__(_("All Applications"))
+	
+	def get_items(self):
+		from gio import app_info_get_all
+		# Choosing only item.should_show() items misses all Preference applets
+		# so we use a slight heurestic
+		taken = set()
+		for item in app_info_get_all():
+			if item.should_show():
+				yield AppLeaf(item)
+				taken.add(item.get_executable())
+		# Re-run and take some more
+		for item in app_info_get_all():
+			if (not item.should_show() and item.get_executable() not in taken
+					and (not item.supports_files() and not
+						item.supports_uris())):
+				yield AppLeaf(item)
+				taken.add(item.get_executable())
+
+	def get_description(self):
+		return _("All applications and preferences")
+
+	def get_icon_name(self):
+		return "gnome-applications"
+
