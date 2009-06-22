84a999bc8637b34e89a724daa8723ae80887cdde
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-06-23 01:11:47 +0200

    main: Read config from and defaults from system directories
    Read defaults from a file in ${DATADIR}, and user preferences from a
    file in ~/.config

--- kupfer/main.py
+++ kupfer/main.py
@@ -66,37 +66,21 @@ def get_config():
 	import ConfigParser
 	import os, sys
 
-	cf_file = "kupfer.cfg"
+	from kupfer import config
+
+	config_filename = "kupfer.cfg"
+	defaults_filename = "defaults.cfg"
 	sep = ";"
 	parser = ConfigParser.SafeConfigParser()
-	default_plugins = (
-			"common",
-			"screen",
-			"windows",
-			"applications",
-			"documents",
-		)
-	default_plugins_catalog = (
-			"epiphany",
-			"firefox",
-		)
 	default_directories = ("~/", "~/Desktop", )
+	# Minimal "defaults" to define all fields
+	# Read defaults defined in a defaults.cfg file
 	defaults = {
-		"Plugins": {
-			"Direct" : default_plugins,
-			"Catalog" : default_plugins_catalog,
-		},
-		"Directories" : {
-			"Direct" : default_directories,
-			"Catalog" : (),
-		},
-		"DeepDirectories" : {
-			"Direct" : (),
-			"Catalog" : (),
-			"Depth" : 2,
-		},
+		"Plugins": { "Direct" : (), "Catalog" : (), },
+		"Directories" : { "Direct" : default_directories, "Catalog" : (), },
+		"DeepDirectories" : { "Direct" : (), "Catalog" : (), "Depth" : 1, },
 	}
-	# Set up defaults
+
 	def fill_parser(parser, defaults):
 		for secname, section in defaults.iteritems():
 			if not parser.has_section(secname):
@@ -108,14 +92,33 @@ def get_config():
 					default = str(default)
 				parser.set(secname, key, default)
 
+	# Set up defaults
 	fill_parser(parser, defaults)
-	try:
-		fil = open(cf_file, "r")
-		parser.readfp(fil)
-		fil.close()
-	except IOError:
-		pass
 
+	# Read all config files
+	config_files = []
+	defaults_path = config.get_data_file(defaults_filename)
+	if not defaults_path:
+		# try local file
+		defaults_path = os.path.join("data", defaults_filename)
+	if not os.path.exists(defaults_path):
+		print "Error: no default config file %s found!" % defaults_filename
+	else:
+		config_files += (defaults_path, )
+
+	config_path = config.get_config_file(config_filename)
+	if config_path:
+		config_files += (config_path, )
+
+	for config_file in config_files:
+		try:
+			fil = open(config_file, "r")
+			parser.readfp(fil)
+			fil.close()
+		except IOError, e:
+			print "Error reading configuration file %s: %s", (config_file, e)
+
+	# Read parsed file into the dictionary again
 	for secname, section in defaults.iteritems():
 		for key, default in section.iteritems():
 			value = parser.get(secname, key)
@@ -130,12 +133,6 @@ def get_config():
 				retval = str(value)
 			defaults[secname][key] = retval
 
-	print "Plugins:", defaults["Plugins"]["Direct"], defaults["Plugins"]["Catalog"]
-	print "Directories:", defaults["Directories"]["Direct"]
-	fill_parser(parser, defaults)
-	fil = open(cf_file, "w")
-	parser.write(fil)
-	fil.close()
 	return defaults
 
 def main():
