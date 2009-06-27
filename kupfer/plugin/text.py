cc827f23182122cacd34ee7592f763d555a14f10
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-06-27 20:43:55 +0200

    plugin.text: Add filesystem path matching to text

--- kupfer/plugin/text.py
+++ kupfer/plugin/text.py
@@ -1,8 +1,9 @@
+from os import path, access, R_OK
 
-from kupfer.objects import TextSource, TextLeaf
+from kupfer.objects import TextSource, TextLeaf, FileLeaf
 
 __kupfer_sources__ = ()
-__kupfer_text_sources__ = ("BasicTextSource",)
+__kupfer_text_sources__ = ("BasicTextSource", "PathTextSource", )
 __description__ = _("Text queries")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
@@ -17,3 +18,17 @@ class BasicTextSource (TextSource):
 			return
 		yield TextLeaf(text)
 
+
+class PathTextSource (TextSource):
+	"""Return existing full paths if typed"""
+	def __init__(self):
+		TextSource.__init__(self, name=_("Filesystem text matches"))
+
+	def get_rank(self):
+		return 80
+	def get_items(self, text):
+		# Find directories or files
+		prefix = path.expanduser("~/")
+		filepath = text if path.isabs(text) else path.join(prefix, text)
+		if access(filepath, R_OK):
+			yield FileLeaf(filepath)
