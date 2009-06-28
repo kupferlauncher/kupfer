f8ee0ffa8844f41fd13691a63f408b973cb6946e
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-06-28 06:01:33 +0200

    plugin.commands: Allow passing options to the commands

--- kupfer/plugin/commands.py
+++ kupfer/plugin/commands.py
@@ -2,13 +2,30 @@ import os
 # import symbols in tight loop to local namespace
 from os import access, R_OK, X_OK, path
 
-from kupfer.objects import TextSource, FileLeaf
+from kupfer.objects import TextSource, FileLeaf, Leaf, Execute
+from kupfer import utils, icons
 
 __kupfer_sources__ = ()
 __kupfer_text_sources__ = ("CommandTextSource",)
 __description__ = _("Execute programs in $PATH")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
+
+class Command (Leaf):
+	def __init__(self, obj, name):
+		Leaf.__init__(self, obj, name)
+		self.args = " ".join(name.split(" ", 1)[1:])
+
+	def get_actions(self):
+		yield Execute(args=self.args)
+		yield Execute(in_terminal=True, args=self.args)
+
+	def get_description(self):
+		return "%s %s" % (self.object, self.args)
+	def get_gicon(self):
+		icons.get_gicon_for_file(self.object)
+	def get_icon_name(self):
+		return "exec"
 
 class CommandTextSource (TextSource):
 	"""Yield path and command text items """
@@ -21,10 +38,11 @@ class CommandTextSource (TextSource):
 	def get_items(self, text):
 		if not text:
 			return
+		firstword = text.split()[0]
 		# iterate over $PATH directories
 		PATH = os.environ.get("PATH") or os.defpath
 		for execdir in PATH.split(os.pathsep):
-			exepath = path.join(execdir, text)
+			exepath = path.join(execdir, firstword)
 			if access(exepath, R_OK | X_OK) and path.isfile(exepath):
-				yield FileLeaf(exepath)
+				yield Command(exepath, text)
 				break
