a589d3ded12c1599f00ff7084f36a09fc88cad6c
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-07-01 18:53:39 +0200

    kupfer-activate.sh: Use python -m kupfer.__init__
    Python's "runpy" allowing to run modules dropped(!) the possibility to
    run packages in python2.6. We have to run kupfer.__init__..

--- kupfer-activate.sh
+++ kupfer-activate.sh
@@ -6,4 +6,4 @@ test ${PYTHON:0:1} = "@" && PYTHON=python
 # Try to spawn kupfer via dbus, else go to python
 dbus-send --print-reply --dest=se.kaizer.kupfer /interface se.kaizer.kupfer.Listener.ShowHide >/dev/null 2>&1
 
-test $? != 0 && exec ${PYTHON} -m kupfer $* || ${PYTHON} -c "import gtk.gdk; gtk.gdk.notify_startup_complete()"
+test $? != 0 && exec ${PYTHON} -m kupfer.__init__ $* || ${PYTHON} -c "import gtk.gdk; gtk.gdk.notify_startup_complete()"
