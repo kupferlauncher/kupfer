103ae25cd20d8f053328b1e6db4b3e95a5676ab6
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-07-03 14:43:02 +0200

    utils: Use gtk.get_current_event_time() for gtk.show_uri

--- kupfer/utils.py
+++ kupfer/utils.py
@@ -101,10 +101,10 @@ def show_path(path):
 
 def show_url(url):
 	"""Open any @url with default viewer"""
-	from gtk import show_uri
+	from gtk import show_uri, get_current_event_time
 	from gtk.gdk import screen_get_default
 	from glib import GError
 	try:
-		show_uri(screen_get_default(), url, 0)
+		show_uri(screen_get_default(), url, get_current_event_time())
 	except GError, e:
 		print "Error in gtk.show_uri:", e
