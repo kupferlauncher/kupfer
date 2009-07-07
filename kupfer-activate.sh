dd4536e50ab1f430158121405b19adc915cffc17
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-07-07 16:41:26 +0200

    kupfer-activate.sh: If executed with arguments, don't call over dbus
    This is just a quick rule, so that "kupfer" alone will call up a
    running instance of kupfer, but any argument given to it will run the
    main application instead; then you can run kupfer --version or --help
    while kupfer is running.

--- kupfer-activate.sh
+++ kupfer-activate.sh
@@ -4,6 +4,7 @@ PYTHON="@PYTHON@"
 test ${PYTHON:0:1} = "@" && PYTHON=python
 
 # Try to spawn kupfer via dbus, else go to python
-dbus-send --print-reply --dest=se.kaizer.kupfer /interface se.kaizer.kupfer.Listener.ShowHide >/dev/null 2>&1
+# but only if there are no arguments
+test -z "$*" && dbus-send --print-reply --dest=se.kaizer.kupfer /interface se.kaizer.kupfer.Listener.ShowHide >/dev/null 2>&1
 
 test $? != 0 && exec ${PYTHON} -m kupfer.__init__ $* || ${PYTHON} -c "import gtk.gdk; gtk.gdk.notify_startup_complete()"
