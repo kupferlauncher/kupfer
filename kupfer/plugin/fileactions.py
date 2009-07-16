541450bcf3b7f468b732a3baceb80f4433f3d4a7
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-07-16 18:45:57 +0200

    plugin.fileactions: Rename trash to fileactions and add MoveTo action
    MoveTo: Move file to new location: not yet implemented

--- kupfer/plugin/fileactions.py
+++ kupfer/plugin/fileactions.py
@@ -4,11 +4,11 @@ from kupfer.objects import FileLeaf, ActionDecorator
 from kupfer import utils
 
 
-__kupfer_name__ = _("Trash")
+__kupfer_name__ = _("File actions")
 __kupfer_sources__ = ()
 __kupfer_text_sources__ = ()
 __kupfer_action_decorator__ = ("Decorator", )
-__description__ = _("Move files to Trash")
+__description__ = _("More file actions")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
@@ -16,7 +16,8 @@ class Decorator (ActionDecorator):
 	def applies_to(self):
 		yield FileLeaf
 	def get_actions(self, leaf=None):
-		return (Trash(), )
+		yield Trash()
+		yield MoveTo()
 
 class Trash (Action):
 	def __init__(self):
@@ -29,4 +30,18 @@ class Trash (Action):
 		return _("Move this file to trash")
 	def get_icon_name(self):
 		return "user-trash-full"
+
+class MoveTo (Action):
+	def __init__(self):
+		Action.__init__(self, _("Move to..."))
+	def activate(self, leaf, obj):
+		print "Want to move", leaf, "to", obj
+
+	def requires_object(self):
+		return True
+
+	def object_types(self):
+		yield FileLeaf
+	def valid_object(self, obj):
+		return obj.is_dir()
 
