fa4510f62fa02ddd167e7085267853116970b59a
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-07-21 00:35:50 +0200

    contrib: Update wikipedia plugin to work with new architecture

--- contrib/wikipedia.py
+++ contrib/wikipedia.py
@@ -1,30 +1,31 @@
-from kupfer.objects import Action, TextLeaf, ActionDecorator
+"""
+This is a simple plugin demonstration, how to add single, simple actions
+"""
+
+import urllib
+
+from kupfer.objects import Action, TextLeaf
 from kupfer import utils
 
+__kupfer_name__ = _("Wikipedia")
 __kupfer_sources__ = ()
-__kupfer_text_sources__ = ()
-__kupfer_action_decorator__ = ("Decorator", )
+__kupfer_actions__ = ("WikipediaSearch", )
 __description__ = _("Send search queries to Wikipedia")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
-
-class Decorator (ActionDecorator):
-	def applies_to(self):
-		yield TextLeaf
-	def get_actions(self, leaf=None):
-		return (WikipediaSearch(), )
 
 class WikipediaSearch (Action):
 	def __init__(self):
 		Action.__init__(self, _("Search in Wikipedia"))
 
 	def activate(self, leaf):
-		from urllib import urlencode
 		# Send in UTF-8 encoding
 		search_url="http://en.wikipedia.org/w/index.php?title=Special:Search&go=Go"
 		# will encode search=text, where `text` is escaped
-		query_url = search_url + "&" + urlencode({"search": leaf.object})
+		query_url = search_url + "&" + urllib.urlencode({"search": leaf.object})
 		utils.show_url(query_url)
+	def item_types(self):
+		yield TextLeaf
 	def get_description(self):
 		return _("Search for this term in en.wikipedia.org")
 	def get_icon_name(self):
