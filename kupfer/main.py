6ac3f6110069072af3dd51f7d52ea2a9ecc89b6a
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-07-24 02:53:35 +0200

    settings: Rebuild to SettingsController object
    Rebuild settings module in the form of a controller object, so we can
    encapsulate more things.

--- kupfer/main.py
+++ kupfer/main.py
@@ -143,31 +143,32 @@ def main():
 		abs = path.abspath(path.expanduser(opt))
 		return objects.FileSource((abs,), depth)
 
-	source_config = settings.get_config()
+	setctl = settings.GetSettingsController()
+	source_config = setctl.get_config
 
 	text_sources = []
 	action_decorators = []
 
-	for item in source_config["Plugins"]["Catalog"]:
+	for item in source_config("Plugins", "Catalog"):
 		s_sources.extend(load_plugin_sources(item))
 		text_sources.extend(load_plugin_sources(item, text_sources_attribute))
 		action_decorators.extend(load_plugin_sources(item,
 			action_decorators_attribute))
-	for item in source_config["Plugins"]["Direct"]:
+	for item in source_config("Plugins", "Direct"):
 		S_sources.extend(load_plugin_sources(item))
 		text_sources.extend(load_plugin_sources(item, text_sources_attribute))
 		action_decorators.extend(load_plugin_sources(item,
 			action_decorators_attribute))
 
-	dir_depth = source_config["DeepDirectories"]["Depth"]
+	dir_depth = source_config("DeepDirectories", "Depth")
 
-	for item in source_config["Directories"]["Catalog"]:
+	for item in source_config("Directories", "Catalog"):
 		s_sources.append(dir_source(item))
-	for item in source_config["DeepDirectories"]["Catalog"]:
+	for item in source_config("DeepDirectories","Catalog"):
 		s_sources.append(file_source(item, dir_depth))
-	for item in source_config["Directories"]["Direct"]:
+	for item in source_config("Directories", "Direct"):
 		S_sources.append(dir_source(item))
-	for item in source_config["DeepDirectories"]["Direct"]:
+	for item in source_config("DeepDirectories", "Direct"):
 		S_sources.append(file_source(item, dir_depth))
 	
 	if not S_sources and not s_sources:
@@ -178,8 +179,8 @@ def main():
 	dc.register_text_sources(text_sources)
 	dc.register_action_decorators(action_decorators)
 	w = browser.WindowController()
-	w.register_keybinding(source_config["Kupfer"]["Keybinding"])
-	show_icon = not (source_config["Kupfer"]["ShowStatusIcon"].lower() not in
+	w.register_keybinding(source_config("Kupfer", "Keybinding"))
+	show_icon = not (source_config("Kupfer", "ShowStatusIcon").lower() not in
 			("yes", "true", ))
 	w.set_show_statusicon(show_icon)
 
--- kupfer/settings.py
+++ kupfer/settings.py
@@ -3,17 +3,10 @@ import ConfigParser
 
 from kupfer import config
 
-def get_config():
-	"""
-	Read cascading config files
-	default -> then config
-	(in all XDG_CONFIG_DIRS)
-	"""
-
+class SettingsController (object):
 	config_filename = "kupfer.cfg"
 	defaults_filename = "defaults.cfg"
 	sep = ";"
-	parser = ConfigParser.SafeConfigParser()
 	default_directories = ("~/", "~/Desktop", )
 	# Minimal "defaults" to define all fields
 	# Read defaults defined in a defaults.cfg file
@@ -23,58 +16,77 @@ def get_config():
 		"Directories" : { "Direct" : default_directories, "Catalog" : (), },
 		"DeepDirectories" : { "Direct" : (), "Catalog" : (), "Depth" : 1, },
 	}
+	def __init__(self):
+		self.config = self._read_config()
+	def _read_config(self):
+		"""
+		Read cascading config files
+		default -> then config
+		(in all XDG_CONFIG_DIRS)
+		"""
 
-	def fill_parser(parser, defaults):
-		for secname, section in defaults.iteritems():
-			if not parser.has_section(secname):
-				parser.add_section(secname)
+		parser = ConfigParser.SafeConfigParser()
+
+		def fill_parser(parser, defaults):
+			for secname, section in defaults.iteritems():
+				if not parser.has_section(secname):
+					parser.add_section(secname)
+				for key, default in section.iteritems():
+					if isinstance(default, (tuple, list)):
+						default = self.sep.join(default)
+					elif isinstance(default, int):
+						default = str(default)
+					parser.set(secname, key, default)
+
+		# Set up defaults
+		confmap = dict(self.defaults)
+		fill_parser(parser, confmap)
+
+		# Read all config files
+		config_files = []
+		defaults_path = config.get_data_file(self.defaults_filename)
+		if not defaults_path:
+			# try local file
+			defaults_path = os.path.join("data", self.defaults_filename)
+		if not os.path.exists(defaults_path):
+			print "Error: no default config file %s found!" % self.defaults_filename
+		else:
+			config_files += (defaults_path, )
+
+		config_path = config.get_config_file(self.config_filename)
+		if config_path:
+			config_files += (config_path, )
+
+		for config_file in config_files:
+			try:
+				fil = open(config_file, "r")
+				parser.readfp(fil)
+				fil.close()
+			except IOError, e:
+				print "Error reading configuration file %s: %s", (config_file, e)
+
+		# Read parsed file into the dictionary again
+		for secname, section in confmap.iteritems():
 			for key, default in section.iteritems():
+				value = parser.get(secname, key)
 				if isinstance(default, (tuple, list)):
-					default = sep.join(default)
+					if not value:
+						retval = ()
+					else:
+						retval = [p.strip() for p in value.split(self.sep) if p]
 				elif isinstance(default, int):
-					default = str(default)
-				parser.set(secname, key, default)
-
-	# Set up defaults
-	fill_parser(parser, defaults)
-
-	# Read all config files
-	config_files = []
-	defaults_path = config.get_data_file(defaults_filename)
-	if not defaults_path:
-		# try local file
-		defaults_path = os.path.join("data", defaults_filename)
-	if not os.path.exists(defaults_path):
-		print "Error: no default config file %s found!" % defaults_filename
-	else:
-		config_files += (defaults_path, )
-
-	config_path = config.get_config_file(config_filename)
-	if config_path:
-		config_files += (config_path, )
-
-	for config_file in config_files:
-		try:
-			fil = open(config_file, "r")
-			parser.readfp(fil)
-			fil.close()
-		except IOError, e:
-			print "Error reading configuration file %s: %s", (config_file, e)
-
-	# Read parsed file into the dictionary again
-	for secname, section in defaults.iteritems():
-		for key, default in section.iteritems():
-			value = parser.get(secname, key)
-			if isinstance(default, (tuple, list)):
-				if not value:
-					retval = ()
+					retval = type(default)(value)
 				else:
-					retval = [p.strip() for p in value.split(sep) if p]
-			elif isinstance(default, int):
-				retval = type(default)(value)
-			else:
-				retval = str(value)
-			defaults[secname][key] = retval
+					retval = str(value)
+				confmap[secname][key] = retval
 
-	return defaults
+		return confmap
+	def get_config(self, section, key):
+		return self.config[section][key]
 
+_settings_controller = None
+def GetSettingsController():
+	global _settings_controller
+	if _settings_controller is None:
+		_settings_controller = SettingsController()
+	return _settings_controller
