ecbb6169a17d1192386c586384de1196aa115ce3
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-07-24 03:39:40 +0200

    plugin.wikipedia: Use plugin_support.PluginSettings

--- kupfer/plugin/wikipedia.py
+++ kupfer/plugin/wikipedia.py
@@ -5,7 +5,7 @@ This is a simple plugin demonstration, how to add single, simple actions
 import urllib
 
 from kupfer.objects import Action, TextLeaf
-from kupfer import utils
+from kupfer import utils, plugin_support
 
 __kupfer_name__ = _("Wikipedia")
 __kupfer_sources__ = ()
@@ -14,20 +14,32 @@ __description__ = _("Send search queries to Wikipedia")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
+__kupfer_settings__ = plugin_support.PluginSettings(
+	{
+		"key": "lang",
+		"label": _("Wikipedia language"),
+		"type": str,
+		"value": "en",
+	},
+)
+
+
 class WikipediaSearch (Action):
 	def __init__(self):
 		Action.__init__(self, _("Search in Wikipedia"))
 
 	def activate(self, leaf):
 		# Send in UTF-8 encoding
-		search_url="http://en.wikipedia.org/w/index.php?title=Special:Search&go=Go"
+		lang_code = __kupfer_settings__["lang"]
+		search_url="http://%s.wikipedia.org/w/index.php?title=Special:Search&go=Go" % lang_code
 		# will encode search=text, where `text` is escaped
 		query_url = search_url + "&" + urllib.urlencode({"search": leaf.object})
 		utils.show_url(query_url)
 	def item_types(self):
 		yield TextLeaf
 	def get_description(self):
-		return _("Search for this term in en.wikipedia.org")
+		lang_code = __kupfer_settings__["lang"]
+		return _("Search for this term in %s.wikipedia.org") % lang_code
 	def get_icon_name(self):
 		return "gtk-find"
 
