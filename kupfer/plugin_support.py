e163512b4edc9331dc5d6bc7e8b2483f7e5cafe3
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-07-25 02:35:41 +0200

    plugin_support: PluginSettings docstrings

--- kupfer/plugin_support.py
+++ kupfer/plugin_support.py
@@ -1,8 +1,20 @@
 from kupfer import pretty, settings
 
 class PluginSettings (pretty.OutputMixin):
+	"""Allows plugins to have preferences by assigning an instance
+	of this class to the plugin's __kupfer_settings__ attribute.
+
+	Setting values are accessed by the getitem operator [] with
+	the setting's 'key' attribute
+	"""
 	def __init__(self, *setdescs):
-		"""Check if passed-in dictionaries have all required keys"""
+		"""Create a settings collection by passing in dictionaries
+		as arguments, where each dictionary must have the following keys:
+			key
+			type
+			value (default value)
+			label (localized label)
+		"""
 		self.setting_descriptions = {}
 		req_keys = set(("key", "value", "type", "label"))
 		for desc in setdescs:
