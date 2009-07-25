0963849c7672fecfa52d343a6e4ab352df277ae3
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-07-25 02:16:25 +0200

    plugin_support: Read settings on initialize()
    plugin_support.PluginSetting now has an initialize method to init a
    plugin under a specific name, where it reads its settings.

--- kupfer/plugin_support.py
+++ kupfer/plugin_support.py
@@ -1,7 +1,8 @@
-from kupfer import pretty
+from kupfer import pretty, settings
 
 class PluginSettings (pretty.OutputMixin):
 	def __init__(self, *setdescs):
+		"""Check if passed-in dictionaries have all required keys"""
 		self.setting_descriptions = {}
 		req_keys = set(("key", "value", "type", "label"))
 		for desc in setdescs:
@@ -13,10 +14,18 @@ class PluginSettings (pretty.OutputMixin):
 	def __iter__(self):
 		return iter(self.setting_descriptions)
 
+	def initialize(self, plugin_name):
+		"""Init by reading from global settings and setting up callbacks"""
+		setctl = settings.GetSettingsController()
+		# FIXME: We should set up preference key changed callbacks here
+		for key in self:
+			value_type = self.setting_descriptions[key]["type"]
+			value = setctl.get_plugin_config(plugin_name, key, value_type)
+			if value is not None:
+				self[key] = value
+
 	def __getitem__(self, key):
-		self.output_debug("Getting", key)
 		return self.setting_descriptions[key]["value"]
 	def __setitem__(self, key, value):
 		value_type = self.setting_descriptions[key]["type"]
 		self.setting_descriptions[key]["value"] = value_type(value)
-
