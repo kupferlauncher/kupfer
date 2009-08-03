a0d09bd161610efe44e7a901f663f945fcc89b26
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-08-04 00:36:09 +0200

    preferences: Basics to handle signals

--- kupfer/preferences.py
+++ kupfer/preferences.py
@@ -13,11 +13,25 @@ class PreferencesWindowController (object):
 		else:
 			self.window = None
 			return
+		builder.connect_signals(self)
 		self.window = builder.get_object("preferenceswindow")
+		self.window.set_position(gtk.WIN_POS_CENTER)
+		self.window.connect("delete-event", self._close_window)
+	def on_checkstatusicon_toggled(self, widget):
+		pass
+	def on_checkautostart_toggled(self, widget):
+		pass
+	def on_entrykeybinding_changed(self, widget):
+		pass
+	def on_closebutton_clicked(self, widget):
+		self.hide()
 	def show(self):
 		self.window.show()
 	def hide(self):
 		self.window.hide()
+	def _close_window(self, *ignored):
+		self.hide()
+		return True
 
 _preferences_window = None
 
