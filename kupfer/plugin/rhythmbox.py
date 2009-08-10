8375416b01e7a8dbe201049dffc59cafea76e41f
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-08-10 20:09:23 +0200

    plugin.rhythmbox: More WIP rhythmbox

--- kupfer/plugin/rhythmbox.py
+++ kupfer/plugin/rhythmbox.py
@@ -8,11 +8,30 @@ __description__ = _("Rhythmbox")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
-
-class AlbumLeaf (Leaf):
+class SongLeaf (Leaf):
 	def __init__(self, name, artist, info):
 		Leaf.__init__(self, info, name)
 		self.artist = artist
+	def get_icon_name(self):
+		return "audio-x-generic"
+
+class AlbumSource (Source):
+	def __init__(self, name, info):
+		Source.__init__(self, name)
+		self.info = info
+	def get_items(self):
+		for song in self.info:
+			yield SongLeaf(song["title"], song["artist"], song)
+	def get_icon_name(self):
+		return "media-optical"
+
+class AlbumLeaf (Leaf):
+	def __init__(self, name, info):
+		Leaf.__init__(self, info, name)
+	def has_content(self):
+		return True
+	def content_source(self, alternate=False):
+		return AlbumSource(unicode(self), self.object)
 	def get_icon_name(self):
 		return "media-optical"
 
@@ -23,10 +42,9 @@ class RhythmboxAlbumsSource (Source):
 		Source.__init__(self, _("Rhythmbox Albums"))
 	
 	def get_items(self):
-		library = rhythmbox_support.get_rhythmbox_artist_albums()
-		for artist in library:
-			for album in library[artist]:
-				yield AlbumLeaf(album, artist, library[artist][album])
+		library = rhythmbox_support.get_rhythmbox_albums()
+		for album in library:
+			yield AlbumLeaf(album, library[album])
 	def should_sort_lexically(self):
 		return True
 
