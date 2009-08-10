3a24ce9cdcd6c99dda3bfa6fa22c9c4f931a140c
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-08-10 20:40:42 +0200

    plugin.rhythmbox: Implement Play and Enqueue for Songs

--- kupfer/plugin/rhythmbox.py
+++ kupfer/plugin/rhythmbox.py
@@ -1,5 +1,7 @@
+import gio
+
 from kupfer.objects import Leaf, Source, AppLeaf, Action, FileLeaf
-from kupfer import objects
+from kupfer import objects, icons, utils
 from kupfer.plugin import rhythmbox_support
 
 __kupfer_name__ = _("Rhythmbox")
@@ -8,10 +10,47 @@ __description__ = _("Rhythmbox")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
+def _tostr(ustr):
+	return ustr.encode("UTF-8")
+
+class Play (Action):
+	rank_adjust = 5
+	def __init__(self):
+		Action.__init__(self, _("Play"))
+	def activate(self, leaf):
+		if isinstance(leaf, SongLeaf):
+			uri = _tostr(leaf.object["location"])
+			utils.spawn_async(("rhythmbox-client", "--play-uri=%s" % uri))
+
+	def get_icon_name(self):
+		return "media-playback-start"
+
+class Enqueue (Action):
+	def __init__(self):
+		Action.__init__(self, _("Enqueue"))
+	def activate(self, leaf):
+		if isinstance(leaf, SongLeaf):
+			uri = _tostr(leaf.object["location"])
+			gfile = gio.File(uri)
+			path = gfile.get_path()
+			utils.spawn_async(("rhythmbox-client", "--enqueue", path))
+	def get_description(self):
+		return _("Put song in the queue")
+	def get_gicon(self):
+		return icons.ComposedIcon("gtk-execute", "media-playback-start")
+	def get_icon_name(self):
+		return "media-playback-start"
+
 class SongLeaf (Leaf):
 	def __init__(self, name, artist, info):
 		Leaf.__init__(self, info, name)
 		self.artist = artist
+	def get_actions(self):
+		yield Play()
+		yield Enqueue()
+	def get_description(self):
+		# TRANS: Song description
+		return _("%s by %s") % (unicode(self), self.artist)
 	def get_icon_name(self):
 		return "audio-x-generic"
 
@@ -36,8 +75,6 @@ class AlbumLeaf (Leaf):
 		return "media-optical"
 
 class RhythmboxAlbumsSource (Source):
-	"""
-	"""
 	def __init__(self):
 		Source.__init__(self, _("Rhythmbox Albums"))
 	
