8038c7ebcd94052f6f34280fcbb459ba7f5dfa8e
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-08-12 00:19:31 +0200

    kupfer-activate.sh: Rewrite to send queries with dbus from cli

--- kupfer-activate.sh
+++ kupfer-activate.sh
@@ -1,10 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 PYTHON="@PYTHON@"
-test ${PYTHON} = @"PYTHON"@ && PYTHON=python
+test ${PYTHON:0:1} = "@" && PYTHON=python
 
 # Try to spawn kupfer via dbus, else go to python
-# but only if there are no arguments
-test -z "$*" && dbus-send --print-reply --dest=se.kaizer.kupfer /interface se.kaizer.kupfer.Listener.ShowHide >/dev/null 2>&1
 
-test $? != 0 && exec ${PYTHON} -m kupfer.__init__ $* || ${PYTHON} -c "import gtk.gdk; gtk.gdk.notify_startup_complete()"
+# Figure out if there are any options "--help" etc, then launch kupfer
+# If there are any non-option arguments, send them to kupfer with PutText
+test "x${1:0:2}" = "x--"
+KUPFER_HAS_OPTIONS=$?
+test -n "$*"
+KUPFER_HAS_CLIARGS=$?
+
+test $KUPFER_HAS_OPTIONS != 0 && dbus-send --print-reply --dest=se.kaizer.kupfer /interface se.kaizer.kupfer.Listener.ShowHide >/dev/null 2>&1
+KUPFER_RUNNING=$?
+
+test \( $KUPFER_HAS_CLIARGS = 0 -a $KUPFER_HAS_OPTIONS != 0 \) && dbus-send --print-reply --dest=se.kaizer.kupfer /interface se.kaizer.kupfer.Listener.PutText string:"$PWD" string:"$*" >/dev/null 2>&1
+
+test $KUPFER_RUNNING != 0 && exec ${PYTHON} -m kupfer.__init__ $* || ${PYTHON} -c "import gtk.gdk; gtk.gdk.notify_startup_complete()"
