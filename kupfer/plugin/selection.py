e798c04b26db6143818f01dd173dfee3f7a64175
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-08-12 08:44:47 +0200

    plugin.selection: Use a hack to hide the selection source
    Use a hack, where we return in get_leaf_repr a leaf that's always
    invalid and won't be shown.

--- kupfer/plugin/selection.py
+++ kupfer/plugin/selection.py
@@ -1,6 +1,6 @@
 import gtk
 
-from kupfer.objects import Source, Leaf, TextLeaf
+from kupfer.objects import Source, Leaf, TextLeaf, SourceLeaf
 
 __kupfer_name__ = _("Selected Text")
 __kupfer_sources__ = ("SelectionSource", )
@@ -11,6 +11,11 @@ __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 class SelectedText (TextLeaf):
 	def __init__(self, text):
 		TextLeaf.__init__(self, text, _("Selected Text"))
+
+class InvisibleSourceLeaf (SourceLeaf):
+	"""Hack to hide this source"""
+	def is_valid(self):
+		return False
 
 class SelectionSource (Source):
 	def __init__(self):
@@ -28,3 +33,5 @@ class SelectionSource (Source):
 		return _("Provides currently Selected text")
 	def provides(self):
 		yield TextLeaf
+	def get_leaf_repr(self):
+		return InvisibleSourceLeaf(self)
