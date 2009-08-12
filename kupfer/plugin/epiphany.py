64df286243da6278734aed8f4461eacdd093a024
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-08-12 17:07:15 +0200

    plugin.epiphany, plugin.firefox: Add bookmarks as content to application

--- kupfer/plugin/epiphany.py
+++ kupfer/plugin/epiphany.py
@@ -1,13 +1,15 @@
-from kupfer.objects import Leaf, Action, Source
+from kupfer.objects import Leaf, Action, Source, AppLeafContentMixin
 from kupfer.objects import UrlLeaf
 
 __kupfer_name__ = _("Epiphany Bookmarks")
 __kupfer_sources__ = ("EpiphanySource", )
+__kupfer_contents__ = ("EpiphanySource", )
 __description__ = _("Index of Epiphany bookmarks")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
-class EpiphanySource (Source):
+class EpiphanySource (AppLeafContentMixin, Source):
+	appleaf_content_id = "epiphany.desktop"
 	def __init__(self):
 		super(EpiphanySource, self).__init__(_("Epiphany Bookmarks"))
 	
--- kupfer/plugin/firefox.py
+++ kupfer/plugin/firefox.py
@@ -1,13 +1,15 @@
-from kupfer.objects import Leaf, Action, Source
+from kupfer.objects import Leaf, Action, Source, AppLeafContentMixin
 from kupfer.objects import UrlLeaf
 
 __kupfer_name__ = _("Firefox Bookmarks")
 __kupfer_sources__ = ("BookmarksSource", )
+__kupfer_contents__ = ("BookmarksSource", )
 __description__ = _("Index of Firefox bookmarks")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
-class BookmarksSource (Source):
+class BookmarksSource (AppLeafContentMixin, Source):
+	appleaf_content_id = ("firefox.desktop", "iceweasel.desktop")
 	def __init__(self):
 		super(BookmarksSource, self).__init__(_("Firefox Bookmarks"))
 	
@@ -18,7 +20,8 @@ class BookmarksSource (Source):
 
 	def get_description(self):
 		return _("Index of Firefox bookmarks")
-
+	def get_gicon(self):
+		return self.get_leaf_repr() and self.get_leaf_repr().get_gicon()
 	def get_icon_name(self):
 		return "web-browser"
 	def provides(self):
