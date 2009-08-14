443431495fa8f5db8ceb87da4e49c82922cbfd37
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-08-14 16:32:51 +0200

    search: Remove unused code for sorting
    Rankables shouldn't care about sorting anymore (more than after their
    rank). So we remove most uneeded code in the search module

--- kupfer/search.py
+++ kupfer/search.py
@@ -8,12 +8,9 @@ from kupfer.relevance import score
 def make_rankables(itr, rank=0):
 	return (Rankable(unicode(obj), obj, rank) for obj in itr)
 
-def make_alpharankables(itr, rank=0):
-	"""make rankables that sort alphabetically"""
-	return (AlphaRankable(unicode(obj), obj, rank) for obj in itr)
-def make_nosortrankables(itr, rank=0):
-	"""make rankables that do not sort """
-	return (NoSortRankable(unicode(obj), obj, rank) for obj in itr)
+def locale_rankable_cmp(me, other):
+	p1 = -cmp(me.rank, other.rank)
+	return p1 or (me.rank and locale.strcoll(me.value, other.value))
 
 class Rankable (object):
 	"""
@@ -37,33 +34,11 @@ class Rankable (object):
 	def __eq__(self, other):
 		return (self.object == self.object)
 
-	def __cmp__(self, other):
-		if isinstance(other, Rankable):
-			p1 = -cmp(self.rank, other.rank)
-			p2 = 0
-			if p1 == 0 and self.rank:
-				p2 = locale.strcoll(self.value, other.value)
-			return p1 + p2
-		return -1
-	
 	def __str__(self):
 		return "%s: %s" % (self.rank, self.value)
 
 	def __repr__(self):
 		return "<Rankable %s repres %s at %x>" % (str(self), repr(self.object), id(self))
-
-class AlphaRankable (Rankable):
-	"""Like rankable but sorts alphabetically"""
-	def __cmp__(self, other):
-		if isinstance(other, Rankable):
-			return locale.strcoll(self.value, other.value)
-		return -1
-class NoSortRankable (Rankable):
-	"""Like rankable but all items equal"""
-	def __cmp__(self, other):
-		if isinstance(other, Rankable):
-			return 0
-		return -1
 
 def bonus_objects(rankables, key):
 	"""generator of @rankables that have mnemonics for @key
