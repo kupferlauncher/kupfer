05485282da92666e1024a1d3a353ecef973556f9
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-08-14 14:18:55 +0200

    keybindings: Allow unbinding completely
    Allow the user to configure kupfer without keybinding, simply by
    leaving the keybinding field blank.

--- kupfer/keybindings.py
+++ kupfer/keybindings.py
@@ -13,14 +13,14 @@ def GetKeyboundObject():
 	return _keybound_object
 
 class KeyboundObject (gobject.GObject):
-	__gtype_name__ = "KeyboundObject"
 	"""Keybinder object
 
 	signals:
 		keybinding (target, event_time)
-		keybinding signal is triggered when the key bound for @target is
-		triggered.
+		keybinding signal is triggered when the key bound
+		for @target is triggered.
 	"""
+	__gtype_name__ = "KeyboundObject"
 	def __init__(self):
 		super(KeyboundObject, self).__init__()
 	def _keybinding(self, target):
@@ -40,30 +40,35 @@ def get_currently_bound_key(target=KEYBINDING_DEFAULT):
 	return _currently_bound.get(target)
 
 def bind_key(keystr, keybinding_target=KEYBINDING_DEFAULT):
-	""" bind key, unbinding any previous key for @keybinding_target"""
+	"""
+	Bind @keystr, unbinding any previous key for @keybinding_target.
+	If @keystr is a false value, any previous key will be unbound.
+	"""
 	try:
 		import keybinder
 	except ImportError:
 		pretty.print_error("Could not import keybinder, keybindings disabled!")
-	else:
-		callback = lambda : GetKeyboundObject()._keybinding(keybinding_target)
-		keybinding_target = int(keybinding_target)
-		if len(keystr) < 2:
-			pretty.print_error(__name__, "Refusing to bind key", keystr)
-			return False
+		return False
 
+	keybinding_target = int(keybinding_target)
+	callback = lambda : GetKeyboundObject()._keybinding(keybinding_target)
+	if len(keystr) == 1:
+		pretty.print_error(__name__, "Refusing to bind key", repr(keystr))
+		return False
+
+	succ = True
+	if keystr:
 		try:
 			succ = keybinder.bind(keystr, callback)
-			pretty.print_debug(__name__, "binding", keystr)
+			pretty.print_debug(__name__, "binding", repr(keystr))
 		except KeyError, exc:
 			pretty.print_error(__name__, exc)
 			succ = False
-		if succ:
-			old_keystr = get_currently_bound_key(keybinding_target)
-			if old_keystr and old_keystr != keystr:
-				keybinder.unbind(old_keystr)
-				pretty.print_debug(__name__, "unbinding", old_keystr)
-			_register_bound_key(keystr, keybinding_target)
-		return succ
-	return False
+	if succ or not keystr:
+		old_keystr = get_currently_bound_key(keybinding_target)
+		if old_keystr and old_keystr != keystr:
+			keybinder.unbind(old_keystr)
+			pretty.print_debug(__name__, "unbinding", repr(old_keystr))
+		_register_bound_key(keystr, keybinding_target)
+	return succ
 
