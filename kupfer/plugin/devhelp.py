2dd662b2bf838347744979455cfbb3c8db636c94
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-08-15 16:34:54 +0200

    plugin.devhelp: Search in Devhelp action

---
+++ kupfer/plugin/devhelp.py
@@ -0,0 +1,24 @@
+from kupfer.objects import Source, Action, TextLeaf
+from kupfer import utils
+
+__kupfer_name__ = _("Devhelp")
+__kupfer_actions__ = ("LookUp", )
+__description__ = _("Search in Devhelp")
+__version__ = ""
+__author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
+
+class LookUp (Action):
+	def __init__(self):
+		Action.__init__(self, _("Search in Devhelp"))
+	def activate(self, leaf):
+		text = leaf.object
+		utils.launch_commandline("devhelp --search='%s'" % text)
+	def item_types(self):
+		yield TextLeaf
+	def valid_for_item(self, leaf):
+		text = leaf.object
+		return len(text.splitlines()) <= 1
+	def get_description(self):
+		return None
+	def get_icon_name(self):
+		return "devhelp"
