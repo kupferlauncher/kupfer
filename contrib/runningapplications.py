cd23d8380256742916caf51c703047f70cf9c607
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-09-01 19:04:19 +0200

    contrib: Update runningapplications

--- contrib/runningapplications.py
+++ contrib/runningapplications.py
@@ -1,3 +1,4 @@
+import gio
 
 from kupfer.objects import Source, AppLeaf
 from kupfer import launch
@@ -12,17 +13,14 @@ class RunningApplicationsSource (Source):
 	"""List currently running applications """
 	def __init__(self):
 		Source.__init__(self, _("Running Applications"))
-		from gio.unix import desktop_app_info_set_desktop_env
-		desktop_app_info_set_desktop_env("GNOME")
+		self.all_apps = gio.app_info_get_all()
+
 	def is_dynamic(self):
 		return True
 
 	def get_items(self):
-		from gio import app_info_get_all
-		svc = launch.GetApplicationsMatcherService()
-		is_running = svc.application_is_running
-		return (AppLeaf(ai) for ai in app_info_get_all()
-				if is_running(ai.get_id()))
+		is_running = launch.application_is_running
+		return (AppLeaf(ai) for ai in self.all_apps if is_running(ai))
 
 	def get_description(self):
 		return _("Running applications")
