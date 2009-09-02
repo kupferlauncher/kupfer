fd79a0babc7dc6b477bec3430c92828fbb004016
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-09-02 03:37:07 +0200

    helplib: WeakCallback handle disconnecting gobject or dbus

--- kupfer/helplib.py
+++ kupfer/helplib.py
@@ -13,23 +13,28 @@ class WeakCallback (object):
 
 	This allows to connect to gobject signals without it keeping
 	the connecting object alive forever.
+
+	Will use @gobject_token or @dbus_token if set as follows:
+		sender.disconnect(gobject_token)
+		dbus_token.remove()
 	"""
 	def __init__(self, obj, attr):
 		"""Create a new Weak Callback calling the method @obj.@attr"""
 		self.wref = weakref.ref(obj)
 		self.callback_attr = attr
+		self.gobject_token = None
+		self.dbus_token = None
 
 	def __call__(self, *args, **kwargs):
 		obj = self.wref()
 		if obj:
 			attr = getattr(obj, self.callback_attr)
 			attr(*args, **kwargs)
-		else:
-			self.default_callback(*args, **kwargs)
-
-	def default_callback(self, *args, **kwargs):
-		"""The default callback will replace
-		the callback if the object is deleted
-		"""
-		pass
+		elif self.gobject_token:
+			sender = args[0]
+			sender.disconnect(self.gobject_token)
+			self.gobject_token = None
+		elif self.dbus_token:
+			self.dbus_token.remove()
+			self.dbus_token = None
 
