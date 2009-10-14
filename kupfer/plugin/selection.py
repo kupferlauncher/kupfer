c1e34a7a4ada9daf179773018bbfcbd2e6c9c9df
Karol Będkowski <karol.bedkowsk+gh@gmail.com>
2009-10-14 19:12:40 +0200

    plugin.selection: +recognize url in selected text

--- kupfer/plugin/selection.py
+++ kupfer/plugin/selection.py
@@ -1,6 +1,6 @@
 import gtk
 
-from kupfer.objects import Source, Leaf, TextLeaf, SourceLeaf, PicklingHelperMixin
+from kupfer.objects import Source, Leaf, TextLeaf, SourceLeaf, PicklingHelperMixin, UrlLeaf
 from kupfer import objects
 from kupfer.helplib import gobject_connect_weakly
 
@@ -10,20 +10,54 @@ __description__ = _("Provides current selection")
 __version__ = ""
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
+
+import urlparse
+import re
+
+def _check_url(string):
+	''' simple url validate '''
+	url = urlparse.urlparse(string)
+	if not (url.netloc or url.path):
+		return False
+
+	if url.scheme or url.netloc:
+		return True
+
+	return re.match(r'^[-A-Za-z0-9+_.]+\.[A-Za-z]{2,}$', url.path)
+
+def _prepare_summary(text):
+	text = objects.tounicode(text)
+	lines = filter(None, text.splitlines())
+	summary = lines[0] if lines else text
+	maxlen = 10
+	if len(summary) > maxlen:
+		summary = summary[:maxlen] + u".."
+
+	return summary
+
+
 class SelectedText (TextLeaf):
 	qf_id = "selectedtext"
+
 	def __init__(self, text):
-		text = objects.tounicode(text)
-		lines = filter(None, text.splitlines())
-		summary = lines[0] if lines else text
-		maxlen = 10
-		if len(summary) > maxlen:
-			summary = summary[:maxlen] + u".."
+		summary = _prepare_summary(text)
 		TextLeaf.__init__(self, text, _('Selected Text "%s"') % summary)
 
 	def rank_key(self):
 		# return a constant rank key despite the changing name
 		return _("Selected Text")
+
+
+class SelectedUrl (UrlLeaf):
+
+	def __init__(self, text):
+		summary = _prepare_summary(text)
+		UrlLeaf.__init__(self, text, _('Selected URL "%s"') % summary)
+
+	def rank_key(self):
+		# return a constant rank key despite the changing name
+		return _("Selected Text")
+
 
 class InvisibleSourceLeaf (SourceLeaf):
 	"""Hack to hide this source"""
@@ -46,11 +80,16 @@ class SelectionSource (Source, PicklingHelperMixin):
 
 	def get_items(self):
 		if self._text:
-			yield SelectedText(self._text)
+			if _check_url(self._text):
+				yield SelectedUrl(self._text)
+
+			else:
+				yield SelectedText(self._text)
 
 	def get_description(self):
 		return _("Provides current selection")
 	def provides(self):
 		yield TextLeaf
+		yield Leaf
 	def get_leaf_repr(self):
 		return InvisibleSourceLeaf(self)
