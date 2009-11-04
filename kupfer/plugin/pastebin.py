4d25a0421add8fb509e75f73ca1c9aa25fe77941
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2009-11-04 16:09:02 +0100

    pastebin: little cleanup, order the languages

--- kupfer/plugin/pastebin.py
+++ kupfer/plugin/pastebin.py
@@ -1,8 +1,10 @@
+import os
+import urllib
+
 from kupfer.objects import Source, Action, TextLeaf , Leaf
 from kupfer import utils, icons
-import urllib, os, sys, re
 
-__kupfer_name__ = "Pastebin"
+__kupfer_name__ = _("Pastebin")
 __kupfer_actions__ = ("PasteIt", )
 __description__ = ("Paste text in pastebin")
 __version__ = ""
@@ -10,50 +12,49 @@ __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
 website="http://pastebin.com"
 
-_LANGUAGES = {
-		'None': 		'Simple Text',
-		'bash':			'Bash',
-		'c':			'C',
-		'cpp': 			'C++',
-		'html4strict':		'HTML',
-		'java':			'Java',
-		'javascript':		'Javascript',
-		'lua':			'Lua',
-		'perl':			'Perl',
-		'php':			'PHP',	
-		'python':		'Python',
-		'ruby':			'Ruby'	
-}
+_LANGUAGES = [
+	('bash', 'Bash'),
+	('c', 'C'),
+	('cpp', 'C++'),
+	('html4strict', 'HTML'),
+	('java', 'Java'),
+	('javascript', 'Javascript'),
+	('lua', 'Lua'),
+	('perl', 'Perl'),
+	('php', 'PHP'),
+	('python', 'Python'),
+	('ruby', 'Ruby'),
+]
 
 class PasteIt (Action):
 	def __init__(self):
-		Action.__init__(self, "Paste It")
+		Action.__init__(self, _("Pastebin As..."))
+
+	def has_result(self):
+		return True
+
 	def activate(self, leaf, iobj):
 		text = leaf.object
 		#user="user"
-		params=self.getParameters(website,text,iobj.object,user="") #Get the parameters array
-		params=urllib.urlencode(params) #Convert to a format usable with the HTML POST
+		#Get the parameters array
+		params=self.getParameters(website,text,iobj.object,user="")
+		#Convert to a format usable with the HTML POST
+		params=urllib.urlencode(params)
 		print params
 
-		page=urllib.urlopen(website+'/',params) #Send the informations and be redirected to the final page
+		#Send the informations and be redirected to the final page
+		page=urllib.urlopen(website+'/',params)
 		#print page.url
 		return TextLeaf(page.url)
 		
 	def item_types(self):
 		yield TextLeaf
-	def valid_for_item(self, leaf):
-		text = leaf.object
-		return text
+
 	def get_description(self):
-		return "Paste text in pastebin.com"
+		return _("Paste text in pastebin.com")
 
 	def get_gicon(self):
 		return icons.ComposedIcon(self.get_icon_name(), "gtk-paste")
-	
-	def has_result(self):
-		return True
-
-
 
 	def getParameters(self,website,content,code,user):
 		params={
@@ -77,15 +78,6 @@ class PasteIt (Action):
 	def object_source(self, for_item=None):
 		return LanguageSource()
 	    
-	    #params['code2']=content
-	    #params['parent_pid']="" 
-	    #params['format']="text" #The format, for syntax hilighting
-	    #params['paste']="Send" 
-	    #params['remember']="1" #Do you want a cookie ?
-	    #params['expiry']="d" #The expiration, f = forever
-
-    	    #return params
-
 class LanguageLeaf(Leaf):
 	pass
 
@@ -94,11 +86,9 @@ class LanguageSource (Source):
 		Source.__init__(self, "Syntax")
 
 	def get_items(self):
-		for code, name in _LANGUAGES.iteritems():
+		yield LanguageLeaf('None', _('Plain text'))
+		for code, name in _LANGUAGES:
 			yield LanguageLeaf(code,name)
 
 	def provides(self):
 		yield LanguageLeaf
-
-	def should_sort_lexically(self): 
-		return True
