95d0a28ec9eb427247785eabb0964daaeb6144dc
Karol Będkowski <karol.bedkowsk+gh@gmail.com>
2010-01-09 22:00:31 +0100

    grouping: new HostServiceLeaf for ordinary services

--- kupfer/obj/hosts.py
+++ kupfer/obj/hosts.py
@@ -12,6 +12,11 @@ __author__ = ("Ulrik Sverdrup <ulrik.sverdrup@gmail.com>, "
 
 HOST_NAME_KEY = "HOST_NAME"
 HOST_ADDRESS_KEY = "HOST_ADDRESS"
+HOST_SERVICE_NAME_KEY = "HOST_SERVICE_NAME"
+HOST_SERVICE_PORT_KEY = "HOST_SERVICE_PORT"
+HOST_SERVICE_USER_KEY = "HOST_SERVICE_USER"
+HOST_SERVICE_PASS_KEY = "HOST_SERVICE_PASS"
+
 
 class HostLeaf(GroupingLeaf):
 	grouping_slots = (HOST_NAME_KEY, HOST_ADDRESS_KEY)
@@ -19,4 +24,22 @@ class HostLeaf(GroupingLeaf):
 	def get_icon_name(self):
 		return "computer"
 
+
+class HostServiceLeaf(HostLeaf):
+	''' Leaf dedicated for well known services like ftp, ssh, vnc '''
+	def __init__(self, name, address, service, description, port=None, 
+			user=None, password=None ):
+		slots = {
+			HOST_NAME_KEY: name,
+			HOST_ADDRESS_KEY: address,
+			HOST_SERVICE_NAME_KEY: service,
+			HOST_SERVICE_PORT_KEY: port,
+			HOST_SERVICE_USER_KEY: user,
+			HOST_SERVICE_PASS_KEY: password,
+		}
+		HostLeaf.__init__(self, slots, name or address)
+		self._description = description
+
+	def get_description(self):
+		return self._description
 
