d79d1103dd518974f2ee3d87749481e84ce296a3
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2010-01-19 02:44:47 +0100

    contrib: Add a debug plugin
    This is something I spied from Do's sources actually, I don't normally
    read a lot of them. Very clean code I have to say, that evil stuff
    they do sure is inspiring :-) Kupfer could do much better in error
    handling.

---
+++ contrib/evilplugin.py
@@ -0,0 +1,38 @@
+"""
+This is a plugin that should do everything wrong, for debugging Purposes
+"""
+
+from kupfer.objects import Leaf, Action, Source
+
+__kupfer_name__ = u"Evil Plugin"
+__kupfer_sources__ = (
+	"EvilSource",
+	"EvilInstantiationSource",
+)
+__description__ = u"Evil for debugging purposes (necessary evil)"
+__version__ = ""
+__author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
+
+class EvilError (Exception):
+	pass
+
+class EvilInstantiationSource (Source):
+	def __init__(self):
+		raise EvilError
+
+class EvilSource (Source):
+	def __init__(self):
+		Source.__init__(self, u"Evil Source")
+
+	def initialize(self):
+		raise EvilError
+
+	def get_items(self):
+		raise EvilError
+
+	def get_icon_name(self):
+		raise EvilError
+
+	def provides(self):
+		pass
+
