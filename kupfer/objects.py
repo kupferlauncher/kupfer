d79ad5aaa0b2289a931c4822aee99163de322ff2
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2010-02-01 18:47:24 +0100

    Remove transition imports (kupfer.helplib and parts of kupfer.objects)

--- kupfer/objects.py
+++ kupfer/objects.py
@@ -1,9 +1,3 @@
 from kupfer.obj.base import *
 from kupfer.obj.objects import FileLeaf, AppLeaf, UrlLeaf, TextLeaf
 from kupfer.obj.objects import RunnableLeaf, SourceLeaf
-
-# NOTE: VERY TEMPORARY MIGRATION IMPORTS
-# ALL PLUGINS RELYING ON THESE SHOULD BE MIGRATED
-
-from kupfer.obj.helplib import FilesystemWatchMixin, PicklingHelperMixin
-from kupfer.obj.apps import AppLeafContentMixin
