12f0b38ae3bd417ad3b12fa058ea2bad2ca847d9
Karol Będkowski <karol.bedkowsk+gh@gmail.com>
2010-02-03 21:33:05 +0100

    +obj.special: PleaseConfigureLeaf, InvalidCredentialsLeaf
    This leaves can be returned by plugin when proper configuration is missing.
    User can quickly open by them settings window with selected plugin.

---
+++ kupfer/obj/special.py
@@ -0,0 +1,30 @@
+__version__ = '2010-01-21'
+
+
+from kupfer.obj.objects import RunnableLeaf
+from kupfer import kupferui
+
+
+class PleaseConfigureLeaf(RunnableLeaf):
+	""" Show information and allow to open preferences for given plugin """
+	message = _("Please Configure Plugin")
+	description = _("Plugin %s is not configured")
+
+	def __init__(self, plugin_id, plugin_name):
+		RunnableLeaf.__init__(self, plugin_id, self.message)
+		self.plugin_name = plugin_name
+
+	def run(self):
+		kupferui.show_plugin_info(self.object)
+
+	def get_icon_name(self):
+		return 'gtk-preferences'
+
+	def get_description(self):
+		return self.description % self.plugin_name
+
+
+class InvalidCredentialsLeaf(PleaseConfigureLeaf):
+	description = _("Invalid user credentials for %s")
+
+
