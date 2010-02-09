dc32bd62d9dccb71fc8f63050a3d6911c899c5b2
Francesco Marella <francesco.marella@gmail.com>
2010-02-09 20:35:45 +0100

    Add plugin for evolution contact list and email actions

---
+++ kupfer/plugin/evolution.py
@@ -0,0 +1,88 @@
+# -*- coding: UTF-8 -*-
+from __future__ import absolute_import
+
+__kupfer_name__ = _("Evolution")
+__kupfer_sources__ = ("ContactsSource", )
+__kupfer_actions__ = ("NewMailAction", )
+__description__ = _("Evolution contacts")
+__version__ = "2010-02-08"
+__author__ = "Francesco Marella, Karol Będkowski"
+
+import evolution
+
+from kupfer.objects import Action
+from kupfer.objects import TextLeaf, UrlLeaf, RunnableLeaf
+from kupfer import utils
+from kupfer.obj.apps import AppLeafContentMixin
+from kupfer.obj.grouping import ToplevelGroupingSource
+from kupfer.obj.contacts import ContactLeaf, EmailContact, email_from_leaf
+
+class ComposeMail(RunnableLeaf):
+	''' Create new mail without recipient '''
+	def __init__(self):
+		RunnableLeaf.__init__(self, name=_("Compose New Email"))
+
+	def run(self):
+		utils.launch_commandline('evolution mailto:')
+
+	def get_description(self):
+		return _("Compose a new message in Evolution")
+
+	def get_icon_name(self):
+		return "mail-message-new"
+
+
+class NewMailAction(Action):
+	''' Create new mail to selected leaf'''
+	def __init__(self):
+		Action.__init__(self, _('Compose Email'))
+
+	def activate(self, leaf):
+		self.activate_multiple((leaf, ))
+
+	def activate_multiple(self, objects):
+		recipients = ",".join(email_from_leaf(L) for L in objects)
+		utils.spawn_async(["evolution", "mailto:%s" % recipients])
+
+	def get_icon_name(self):
+		return "mail-message-new"
+
+	def item_types(self):
+		yield ContactLeaf
+		# we can enter email
+		yield TextLeaf
+		yield UrlLeaf
+
+	def valid_for_item(self, item):
+		return bool(email_from_leaf(item))
+
+
+class ContactsSource(AppLeafContentMixin, ToplevelGroupingSource):
+	appleaf_content_id = 'evolution'
+
+	def __init__(self, name=_("Evolution Address Book")):
+		super(ContactsSource, self).__init__(name, "Contacts")
+
+	def get_items(self):
+		ebook_ = evolution.ebook.open_addressbook("default")
+		for contact in ebook_.get_all_contacts():
+			name = contact.get_property("full-name")
+			email = contact.get_property("email-1")
+			if email:
+				yield EmailContact(email, name)
+
+		yield ComposeMail()
+
+	def should_sort_lexically(self):
+		return True
+
+	def get_description(self):
+		return _("Evolution contacts")
+
+	def get_icon_name(self):
+		return "evolution"
+
+	def provides(self):
+		yield RunnableLeaf
+		yield ContactLeaf
+
