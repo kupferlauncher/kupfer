5744a0260c07ddb159fa9ea1843cc9cf4eb041b8
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2010-02-13 15:48:47 +0100

    Show all imported names in help(kupfer.objects)

--- kupfer/objects.py
+++ kupfer/objects.py
@@ -1,3 +1,6 @@
 from kupfer.obj.base import *
 from kupfer.obj.objects import FileLeaf, AppLeaf, UrlLeaf, TextLeaf
 from kupfer.obj.objects import RunnableLeaf, SourceLeaf
+
+# Show everything here in help(..)
+__all__ = dir()
