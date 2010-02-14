975c5772f37e1d283c09cbc8cdd1434bf2974d2d
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2010-02-14 18:47:52 +0100

    Move Show Notification action into the Show Text plugin

--- kupfer/plugin/show_text.py
+++ kupfer/plugin/show_text.py
@@ -2,14 +2,16 @@ __kupfer_name__ = _("Show Text")
 __kupfer_actions__ = (
 		"ShowText",
 		"LargeType",
+		"ShowNotification",
 	)
 __description__ = _("Display text in a window")
-__version__ = "0.1"
-__author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
+__version__ = ""
+__author__ = "US"
 
 from kupfer.objects import Action
 from kupfer.objects import TextLeaf
-from kupfer import icons, kupferstring, uiutils
+from kupfer import icons, uiutils
+from kupfer import textutils
 
 
 class ShowText (Action):
@@ -41,6 +43,24 @@ class LargeType (Action):
 		return _("Display text in a window")
 	def get_gicon(self):
 		return icons.ComposedIcon("gtk-bold", "zoom-in")
+	def get_icon_name(self):
+		return "gtk-bold"
+
+class ShowNotification (Action):
+	def __init__(self):
+		Action.__init__(self, _("Show Notification"))
+
+	def activate(self, leaf):
+		title, body = textutils.extract_title_body(leaf.object)
+		if body:
+			uiutils.show_notification(title, body,
+					icon_name=self.get_icon_name())
+		else:
+			uiutils.show_notification(title)
+
+	def item_types(self):
+		yield TextLeaf
+
 	def get_icon_name(self):
 		return "gtk-bold"
 
