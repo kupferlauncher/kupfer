3f44ea4e3ca6ccab358c519fde5c21ce6df12730
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2011-03-15 22:52:31 +0100

    Implement kupfer.obj.exceptions

--- kupfer/objects.py
+++ kupfer/objects.py
@@ -1,4 +1,5 @@
 from kupfer.obj.base import *
+from kupfer.obj.exceptions import *
 from kupfer.obj.objects import FileLeaf, AppLeaf, UrlLeaf, TextLeaf
 from kupfer.obj.objects import RunnableLeaf, SourceLeaf
 
