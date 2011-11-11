1a22caea18ab5e2b1dbb1ff8488ef97b1408f5a9
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2011-11-11 16:30:15 +0100

    duckduckgo: Use name DuckDuckGo Search for plugin. Other small edits.

--- kupfer/plugin/duckduckgo.py
+++ kupfer/plugin/duckduckgo.py
@@ -2,24 +2,24 @@
 This is a DuckDuckGo search plugin based on the Wikipedia search plugin
 """
 
-__kupfer_name__ = _("DuckDuckGo HTTPS Search")
+__kupfer_name__ = _("DuckDuckGo Search")
 __kupfer_sources__ = ()
 __kupfer_actions__ = ("DuckDuckGoSearch",)
-__description__ = _("Search the Web securely with DuckDuckGo")
+__description__ = _("Search the web securely with DuckDuckGo")
 __version__ = "1.0"
 __author__ = "Isaac Aggrey <isaac.aggrey@gmail.com>"
 
 import urllib
 
 from kupfer.objects import Action, TextLeaf
-from kupfer import utils, plugin_support
+from kupfer import utils
 
 class DuckDuckGoSearch (Action):
 	def __init__(self):
 		Action.__init__(self, _("DuckDuckGo Search"))
 
 	def activate(self, leaf):
-		search_url="https://duckduckgo.com/" 
+		search_url = "https://duckduckgo.com/"
 		query_url = search_url + "?" + urllib.urlencode({"q" : leaf.object})
 		utils.show_url(query_url)
 
@@ -27,7 +27,7 @@ class DuckDuckGoSearch (Action):
 		yield TextLeaf
 
 	def get_description(self):
-		return _("Search the Web securely with DuckDuckGo")
+		return _("Search the web securely with DuckDuckGo")
 
 	def get_icon_name(self):
 		return "edit-find"
