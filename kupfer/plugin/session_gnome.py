8bafece44e9ec5216b7def3bf1a1670e4cd0f9a1
Karol Będkowski <karol.bedkowski@gmail.com>
2012-10-16 19:24:43 +0200

    session_gnome: support Gnome3 for logoff/shutdown actions
    Closes: lp#788713
    Author: Joseph Lansdowne

--- kupfer/plugin/session_gnome.py
+++ kupfer/plugin/session_gnome.py
@@ -1,17 +1,27 @@
+# -*- coding: UTF8 -*-
 __kupfer_name__ = _("GNOME Session Management")
 __kupfer_sources__ = ("GnomeItemsSource", )
 __description__ = _("Special items and actions for GNOME environment")
-__version__ = "2009-12-05"
+__version__ = "2012-10-16"
 __author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
+
+"""
+Changes:
+	2012-10-16 Karol Będkowski:
+		+ support Gnome3; closes lp#788713;
+		  author: Joseph Lansdowne
+"""
 
 from kupfer.plugin import session_support as support
 
 
 # sequences of argument lists
 LOGOUT_CMD = (["gnome-panel-logout"],
-              ["gnome-session-save", "--kill"])
+              ["gnome-session-save", "--kill"],
+              ["gnome-session-quit", "--logout"])
 SHUTDOWN_CMD = (["gnome-panel-logout", "--shutdown"],
-                ["gnome-session-save", "--shutdown-dialog"])
+                ["gnome-session-save", "--shutdown-dialog"],
+                ["gnome-session-quit", "--power-off"])
 LOCKSCREEN_CMD = (["gnome-screensaver-command", "--lock"],
                   ["xdg-screensaver", "lock"])
 
