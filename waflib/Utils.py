82410cae43126c50da4260aca7f22544bec5589e
hugosenari <hugosenari@gmail.com>
2017-02-11 22:48:11 -0200

    WAF add fallback to read file as utf8, change to WITH sintax

--- waflib/Utils.py
+++ waflib/Utils.py
@@ -133,17 +133,12 @@ def readf(fname, m='r'):
 	:rtype: string
 	:return: Content of the file
 	"""
-	f = open(fname, m)
 	try:
-		txt = f.read()
-	except:
-		f.close()
-		try:
-			f = open(fname, m, encoding='utf-8')
-			txt = f.read()
-		finally:
-			f.close()
-	return txt
+		with open(fname, m) as f:
+			return f.read()
+	except UnicodeDecodeError:
+		with open(fname, m, encoding='utf-8') as f:
+			return f.read()
 
 def h_file(filename):
 	"""
