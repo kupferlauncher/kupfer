9b369228bfe0abf487b34b86aff4cb1c0af8e632
chaitanya-lakkundi <chaitanya-lakkundi@users.noreply.github.com>
2017-02-15 18:56:26 +0530

    Gnome ColorPicker to set custom colour

---
+++ kupfer/ui/colorpicker.py
@@ -0,0 +1,28 @@
+import sys
+from gi.repository import Gtk as gtk
+#import gtk
+
+def tohex(c):
+   #Convert to hex string
+   #little hack to fix bug
+   s = ['#',hex(int(c[0]*256))[2:].zfill(2),hex(int(c[1]*256))[2:].zfill(2),hex(int(c[2]*256))[2:].zfill(2)]
+   for item in enumerate(s):
+      if item[1]=='100':
+         s[item[0]]='ff'
+   #print s
+   return ''.join(s)
+
+def getColor():
+	csd = gtk.ColorSelectionDialog('Select Colour')
+	response = csd.run()
+	if response == gtk.ResponseType.CANCEL:
+		print("Cancel hit")
+		return None
+	else:
+		print("Selected new color")
+	csd.hide()
+	cs = csd.get_color_selection()
+	c = cs.get_current_color()
+	csd.destroy()		
+	hexColor = tohex((c.red/65536.0, c.green/65536.0, c.blue/65536.0))
+	return hexColor
