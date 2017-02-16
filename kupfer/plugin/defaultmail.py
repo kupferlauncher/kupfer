463fdeaf2e46da49b48c1ff2d2ff17d853745013
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2017-02-16 22:48:15 +0100

    BUG: Disable defaultmail's "attach files usning mailto: handler"
    Thunderbird doesn't support mailto: with ?attach= directives (for
    security reasons). We should just drop this if it's not widely
    supported.
    
    Fixes #42

--- kupfer/plugin/defaultmail.py
+++ kupfer/plugin/defaultmail.py
@@ -1,14 +1,13 @@
 __kupfer_name__ = _("Default Email Client")
 __kupfer_actions__ = (
     "NewMailAction",
-    "SendFileByMail",
 )
 __description__ = _("Compose email using the system's default mailto: handler")
-__version__ = "2010-01-12"
-__author__ = "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
+__version__ = "2017.1"
+__author__ = ""
 
 from kupfer.objects import Action
-from kupfer.objects import TextLeaf, UrlLeaf, FileLeaf
+from kupfer.objects import TextLeaf, UrlLeaf
 from kupfer.obj.contacts import ContactLeaf, email_from_leaf
 from kupfer import utils
 
@@ -37,34 +36,3 @@ class NewMailAction(Action):
         return __description__
     def get_icon_name(self):
         return "mail-message-new"
-
-class SendFileByMail (Action):
-    def __init__(self):
-        Action.__init__(self, _('Send in Email To...'))
-
-    def activate(self, obj, iobj):
-        self.activate_multiple((obj, ), (iobj, ))
-
-    def activate_multiple(self, objects, iobjects):
-        # FIXME: revisit for unicode email addresses
-        recipients = ",".join(email_from_leaf(I) for I in iobjects)
-        attachlist = "?attach=" + "&attach=".join(L.object for L in objects)
-        url = "mailto:" + recipients + attachlist
-        utils.show_url(url)
-
-    def item_types(self):
-        yield FileLeaf
-    def valid_for_item(self, item):
-        return not item.is_dir()
-
-    def requires_object(self):
-        return True
-    def object_types(self):
-        yield ContactLeaf
-        yield TextLeaf
-        yield UrlLeaf
-    def valid_object(self, iobj, for_item=None):
-        return bool(email_from_leaf(iobj))
-
-    def get_icon_name(self):
-        return "document-send"
