2b306a252a0d330656f423bded8e7c2e9fbab720
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2017-04-23 14:46:21 +0200

    ENH: In Firefox Keywords, provide quick search using '?keyword query'
    ? starts free text input. Use firefox keywords for this, so we can use
    ?keyword query to search for something. By default ? strings that don't
    match a keyword search with google.

--- kupfer/plugin/firefox_keywords.py
+++ kupfer/plugin/firefox_keywords.py
@@ -1,5 +1,6 @@
 __kupfer_name__ = _("Firefox Keywords")
 __kupfer_sources__ = ("KeywordsSource", )
+__kupfer_text_sources__ = ("KeywordSearchSource", )
 __kupfer_actions__ = ("SearchWithEngine", )
 __description__ = _("Search the web with Firefox keywords")
 __version__ = "2017.1"
@@ -15,10 +16,18 @@ from kupfer import plugin_support
 from kupfer.objects import Source, Action, Leaf
 from kupfer.objects import TextLeaf, TextSource
 from kupfer.obj.helplib import FilesystemWatchMixin
-from kupfer.obj.objects import OpenUrl
+from kupfer.obj.objects import OpenUrl, RunnableLeaf
 from kupfer import utils
+from kupfer import config, plugin_support
 
-MAX_ITEMS = 10000
+__kupfer_settings__ = plugin_support.PluginSettings(
+    {
+        "key" : "default",
+        "label": _("Default for ?"),
+        "type": str,
+        "value": 'https://www.google.com/search?ie=UTF-8&q=%s',
+    }
+)
 
 def get_firefox_home_file(needed_file):
     firefox_dir = os.path.expanduser("~/.mozilla/firefox")
@@ -75,12 +84,17 @@ class Keyword(Leaf):
         return self.object
 
 class KeywordsSource (Source, FilesystemWatchMixin):
+    instance = None
     def __init__(self):
         super().__init__(_("Firefox Keywords"))
 
     def initialize(self):
+        KeywordsSource.instance = self
         ff_home = get_firefox_home_file('')
         self.monitor_token = self.monitor_directories(ff_home)
+
+    def finalize(self):
+        KeywordsSource.instance = None
 
     def monitor_include_file(self, gfile):
         return gfile and gfile.get_basename() == 'lock'
@@ -105,7 +119,10 @@ class KeywordsSource (Source, FilesystemWatchMixin):
             return []
 
     def get_items(self):
-        return self._get_ffx3_bookmarks()
+        seen_keywords = set()
+        for kw in self._get_ffx3_bookmarks():
+            seen_keywords.add(kw.keyword)
+            yield kw
 
     def get_description(self):
         return None
@@ -183,10 +200,71 @@ class SearchFor (Action):
     def get_icon_name(self):
         return "edit-find"
 
+class KeywordSearchSource(TextSource):
+    def __init__(self):
+        super().__init__(_("Firefox Keywords (?-source)"))
+
+    def get_text_items(self, text):
+        if not text.startswith("?"):
+            return
+        parts = text[1:].split(maxsplit=1)
+        if len(parts) < 1:
+            return
+        query = parts[1] if len(parts) > 1 else ""
+        for kw in KeywordsSource.instance.get_leaves():
+            if kw._is_search() and kw.keyword == parts[0]:
+                yield SearchWithKeyword(kw, query)
+                return
+        default = __kupfer_settings__['default'].strip()
+        if default:
+            if '%s' not in default:
+                default += '%s'
+            yield SearchWithKeyword(Keyword(None, "", default), text[1:])
+
+    def get_description(self):
+        return None
+
+    def get_icon_name(self):
+        return "web-browser"
+
+    def provides(self):
+        yield SearchWithKeyword
+
+    def get_rank(self):
+        return 80
+
+class SearchWithKeyword(RunnableLeaf):
+    def __init__(self, keyword, text):
+        super().__init__((keyword, text), _('Search for "%s"') % (text, ))
+
+    def run(self):
+        kw = self.keyword_leaf
+        _do_search_engine(self.query, kw.object)
+
+    @property
+    def keyword_leaf(self):
+        return self.object[0]
+
+    @property
+    def query(self):
+        return self.object[1]
+
+    def get_icon_name(self):
+        return "web-browser"
+
+    def get_description(self):
+        return _("Search using %s") % self.keyword_leaf
+
+    def get_text_representation(self):
+        kw = self.keyword_leaf
+        return _query_url(self.query, kw.object)
+
 def _do_search_engine(terms, search_url, encoding="UTF-8"):
     """Show an url searching for @search_url with @terms"""
-    #search_url = search_url.encode(encoding, "ignore")
-    #terms_enc = terms.encode(encoding, "ignore")
+    utils.show_url(_query_url(terms, search_url))
+
+def _query_url(terms, search_url):
+    """Show an url searching for @search_url with @terms"""
     query_url = search_url.replace("%s", quote(terms))
-    utils.show_url(query_url)
+    return query_url
 
