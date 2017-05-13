ca3206914e31d3176c5c15ae4dee6d35c8241ab9
Chaitanya Lakkundi <chaitanya.lakkundi@gmail.com>
2017-05-13 20:12:01 +0530

    Add support for custom themes
    The user defined themes are assumed to be present in
    ~/.local/share/kupfer/ directory.
    
    The *.css files are scanned and loaded as choices.

--- kupfer/plugin/custom_theme.py
+++ kupfer/plugin/custom_theme.py
@@ -1,66 +1,76 @@
 __kupfer_name__ = _("Custom Theme")
-__description__ = ""
-__version__ = "2017.1"
+__description__ = "Apply any custom theme"
+__version__ = "2017.5"
 __author__ = "Chaitanya S Lakkundi"
 
 from gi.repository import Gtk, Gdk
 from gi.repository import Gio, GLib
 
 from kupfer import plugin_support, pretty, config
-from shutil import copyfile
+
+from glob import glob
+from os.path import basename
+
+def get_alternative_themes():
+    base_dir = config.get_data_home()
+    css_files = glob(base_dir+"/*.css")
+    themes = [basename(filename[:-4]) for filename in css_files]
+    if themes:
+        return tuple(themes)
+    else:
+        return "None",
 
 __kupfer_settings__ = plugin_support.PluginSettings(
     {
-        "key": "css_file",
+        "key": "theme",
         "label": "Select the theme",
         "type": str,
-        "value": "",
-        "alternatives":(
-            "Light",
-            "Dark",
-            "Custom",
-            )
+        "value": get_alternative_themes()[0],
+        "alternatives": get_alternative_themes()
     },
 )
 
 def initialize_plugin(name):
-    use_theme(__kupfer_settings__['css_file'])
+    try:
+        use_theme(__kupfer_settings__['theme'])
+    except config.ResourceLookupError as e:
+        pretty.print_debug(__name__, e)
+
     __kupfer_settings__.connect_settings_changed_cb(on_change_theme)
 
-def finalize_plugin(name):
-    use_theme(None)
-
 def on_change_theme(sender, key, value):
-    use_theme(value)
+    try:
+        use_theme(value)
+    except config.ResourceLookupError as e:
+        pretty.print_debug(__name__, e)
+        use_theme("light")
+
+def finalize_plugin(name):
+    use_theme('light')
 
 def load_style_css(css_file):
-        pretty.print_debug(__name__,'loading style from css file: ', css_file)
-        css_file = Gio.File.new_for_path(css_file)
-        style_provider = Gtk.CssProvider()
-        Gtk.StyleContext.remove_provider_for_screen(
-                Gdk.Screen.get_default(),
-                style_provider,
-            )
-        try:
-            style_provider.load_from_file(css_file)
-        except GLib.Error:
-            print('Error parsing the css file')
-            return None
-        Gtk.StyleContext.add_provider_for_screen(
-            Gdk.Screen.get_default(),
-            style_provider,
-            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-        )
+    pretty.print_debug(__name__,'loading style from css file: ', css_file)
+    css_file = Gio.File.new_for_path(css_file)
+    style_provider = Gtk.CssProvider()
+    # Gtk.StyleContext.remove_provider_for_screen(
+    #         Gdk.Screen.get_default(),
+    #         style_provider,
+    #     )
+    try:
+        style_provider.load_from_file(css_file)
+    except GLib.Error:
+        pretty.print_debug(__name__, 'Error parsing the css file')
+        return None
 
-#FIXME: Not able to switch back to Light theme in the same running instance
+    Gtk.StyleContext.add_provider_for_screen(
+        Gdk.Screen.get_default(),
+        style_provider,
+        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+    )
+
 def use_theme(theme):
-    if not theme:
-        pretty.print_debug(__name__, "Default theme selected")
-        css_file = config.get_data_file('style.css')
+    try:
+        css_file = config.get_data_file(theme+".css")
         load_style_css(css_file)
-
-    elif theme == "Custom":
-        pass
-    else:
-        css_file = config.get_data_file('style.css.'+theme.lower())
-        load_style_css(css_file)
+    except:
+        pretty.print_debug(__name__, "Theme "+theme+" could not be applied")
