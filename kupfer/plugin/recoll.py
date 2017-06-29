f039ab8e8b9ddced12be905f987a764c19194448
Thorko <webform@thorko.de>
2017-06-29 15:11:20 +0200

    plugin to use recoll search with kupfer

---
+++ kupfer/plugin/recoll.py
@@ -0,0 +1,25 @@
+__kupfer_name__ = _("Recoll search documents")
+__kupfer_actions__ = ("RecollIndex", )
+__description__ = _("Search in recollindex")
+__version__ = "2017-1"
+__author__ = "thorko"
+
+from kupfer.objects import Action, TextLeaf
+from kupfer import utils, pretty
+
+class RecollIndex (Action):
+    def __init__(self):
+        Action.__init__(self, _("Recollindex Search"))
+
+    def activate(self, leaf):
+        search_cmd = ["recoll", "-q", leaf.object]
+        utils.spawn_async(search_cmd)
+
+    def item_types(self):
+        yield TextLeaf
+
+    def get_description(self):
+        return _("Search in your recollindex")
+
+    def get_icon_name(self):
+        return "edit-find"
