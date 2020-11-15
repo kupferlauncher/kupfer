311be57e94c76e0d6d138741c59a1fbb6bbc2c3b
Peter Stuifzand <peter@stuifzand.eu>
2020-11-15 22:06:54 +0100

    Clean up Apt tools plugin

--- kupfer/plugin/apt_tools.py
+++ kupfer/plugin/apt_tools.py
@@ -2,31 +2,31 @@ __kupfer_name__ = _("APT")
 __kupfer_sources__ = ()
 __kupfer_text_sources__ = ()
 __kupfer_actions__ = (
-        "ShowPackageInfo",
-        "SearchPackageName",
-        "InstallPackage",
-    )
+    "ShowPackageInfo",
+    "SearchPackageName",
+    "InstallPackage",
+)
 __description__ = _("Interface with the package manager APT")
 __version__ = ""
-__author__ = ("Martin Koelewijn <martinkoelewijn@gmail.com>, "
-              "Ulrik Sverdrup <ulrik.sverdrup@gmail.com>")
+__author__ = "Martin Koelewijn <martinkoelewijn@gmail.com>, Ulrik Sverdrup <ulrik.sverdrup@gmail.com>"
 
 import os
+import subprocess
 
 from kupfer.objects import Action, Source, Leaf
 from kupfer.objects import TextLeaf
 from kupfer import icons, kupferstring, task, uiutils, utils
 from kupfer import plugin_support
 
-
 __kupfer_settings__ = plugin_support.PluginSettings(
     {
-        "key" : "installation_method",
+        "key": "installation_method",
         "label": _("Installation method"),
         "type": str,
         "value": "gksu -- apt-get install --yes",
     },
 )
+
 
 class InfoTask(task.Task):
     def __init__(self, text):
@@ -39,7 +39,7 @@ class InfoTask(task.Task):
         self._finish_callback = finish_callback
         timeout = 60
         AC = utils.AsyncCommand
-        AC(["aptitude", "show", self.text], self.aptitude_finished, timeout)
+        AC(["apt", "show", self.text], self.aptitude_finished, timeout)
         AC(["apt-cache", "policy", self.text], self.aptcache_finished, timeout)
 
     def aptitude_finished(self, acommand, stdout, stderr):
@@ -55,18 +55,20 @@ class InfoTask(task.Task):
     def _check_end(self):
         if self.aptitude is not None and self.apt_cache is not None:
             self.finish("".join(kupferstring.fromlocale(s)
-                        for s in (self.aptitude, self.apt_cache)))
+                                for s in (self.aptitude, self.apt_cache)))
 
     def finish(self, text):
         uiutils.show_text_result(text, title=_("Show Package Information"))
         self._finish_callback(self)
 
-class ShowPackageInfo (Action):
+
+class ShowPackageInfo(Action):
     def __init__(self):
         Action.__init__(self, _("Show Package Information"))
 
     def is_async(self):
         return True
+
     def activate(self, leaf):
         return InfoTask(leaf.object.strip())
 
@@ -82,12 +84,13 @@ class ShowPackageInfo (Action):
     def get_gicon(self):
         return icons.ComposedIcon("dialog-information", "package-x-generic")
 
-class InstallPackage (Action):
+
+class InstallPackage(Action):
     def __init__(self):
         Action.__init__(self, _("Install"))
 
     def activate(self, leaf):
-        self.activate_multiple((leaf, ))
+        self.activate_multiple((leaf,))
 
     def activate_multiple(self, objs):
         program = (__kupfer_settings__["installation_method"])
@@ -101,22 +104,27 @@ class InstallPackage (Action):
 
     def get_description(self):
         return _("Install package using the configured method")
+
     def get_icon_name(self):
         return "document-save"
 
-class Package (Leaf):
+
+class Package(Leaf):
     def __init__(self, package, desc):
         Leaf.__init__(self, package, package)
         self.desc = desc
 
     def get_text_representation(self):
         return self.object
+
     def get_description(self):
         return self.desc
+
     def get_icon_name(self):
         return "package-x-generic"
 
-class PackageSearchSource (Source):
+
+class PackageSearchSource(Source):
     def __init__(self, query):
         self.query = query
         Source.__init__(self, _('Packages matching "%s"') % query)
@@ -126,30 +134,28 @@ class PackageSearchSource (Source):
 
     def get_items(self):
         package = kupferstring.tolocale(self.query)
-        c_in, c_out_err = os.popen4(['apt-cache', 'search', '--names-only', package])
-        try:
-            c_in.close()
-            acp_out = c_out_err.read()
-            for line in kupferstring.fromlocale(acp_out).splitlines():
-                if not line.strip():
-                    continue
-                if not " - " in line:
-                    self.output_error("apt-cache: ", line)
-                    continue
-                package, desc = line.split(" - ", 1)
-                yield Package(package, desc)
-        finally:
-            c_out_err.close()
+        p = subprocess.run(['apt-cache', 'search', '--names-only', package],
+                           capture_output=True)
+        for line in kupferstring.fromlocale(p.stdout).splitlines():
+            if not line.strip():
+                continue
+            if not " - " in line:
+                self.output_error("apt-cache: ", line)
+                continue
+            package, desc = line.split(" - ", 1)
+            yield Package(package, desc)
 
     def should_sort_lexically(self):
         return True
 
     def provides(self):
         yield TextLeaf
+
     def get_icon_name(self):
         return "system-software-install"
 
-class SearchPackageName (Action):
+
+class SearchPackageName(Action):
     def __init__(self):
         Action.__init__(self, _("Search Package Name..."))
 
@@ -162,6 +168,7 @@ class SearchPackageName (Action):
 
     def item_types(self):
         yield TextLeaf
+
     def valid_for_item(self, item):
         # check if it is a single word
         text = item.object
@@ -169,4 +176,3 @@ class SearchPackageName (Action):
 
     def get_icon_name(self):
         return "system-software-install"
-
