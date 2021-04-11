006f7cc068c39e7a508ce951a54cc1f18aab8535
Karol Będkowski <karol.bedkowski@gmail.com>
2021-04-11 19:31:57 +0200

    session_xfce4: configurable lock screen command
    Allow configure custom lock command as "xdg-screensaver lock" may not work
    in all cases. XFCE provide xflock4 command that may be used instead.
    xflock4 is default value.
    
    Fix: #146

--- kupfer/plugin/session_xfce.py
+++ kupfer/plugin/session_xfce.py
@@ -3,24 +3,36 @@
 __kupfer_name__ = _("XFCE Session Management")
 __kupfer_sources__ = ("XfceItemsSource", )
 __description__ = _("Special items and actions for XFCE environment")
-__version__ = "2009-12-05"
+__version__ = "2021-04-11"
 __author__ = "Karol Będkowski <karol.bedkowski@gmail.com>"
 
 from kupfer.plugin import session_support as support
+from kupfer import plugin_support
 
+__kupfer_settings__ = plugin_support.PluginSettings(
+    {
+        "key": "lock_cmd",
+        "label": _("Screen lock command"),
+        "type": str,
+        "value": "xflock4",
+    },
+)
 
 # sequences of argument lists
 LOGOUT_CMD = (["xfce4-session-logout", "--logout"],)
 SHUTDOWN_CMD = (["xfce4-session-logout"],)
-LOCKSCREEN_CMD = (["xdg-screensaver", "lock"], )
 
 
 class XfceItemsSource (support.CommonSource):
     def __init__(self):
         support.CommonSource.__init__(self, _("XFCE Session Management"))
+
     def get_items(self):
+        lockscreen_cmd = __kupfer_settings__['lock_cmd'] \
+            or "xdg-screensaver lock"
+
         return (
             support.Logout(LOGOUT_CMD),
-            support.LockScreen(LOCKSCREEN_CMD),
+            support.LockScreen((lockscreen_cmd.split(" "), )),
             support.Shutdown(SHUTDOWN_CMD),
         )
