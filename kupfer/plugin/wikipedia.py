19c1bdb5772e87ac5599a8478b2ead32a232f0a0
Karol Będkowski <karol.bedkowski@gmail.com>
2023-03-09 16:59:29 +0100

    feat(plugins): allow define more languages in wikipedia plugin
    Allow select wikipedia language for text search. If its defined only
    one language - use it directly.

--- kupfer/plugin/wikipedia.py
+++ kupfer/plugin/wikipedia.py
@@ -12,12 +12,12 @@ __author__ = "US"
 import urllib.parse
 
 from kupfer import plugin_support, utils
-from kupfer.objects import Action, TextLeaf
+from kupfer.objects import Action, TextLeaf, Leaf, Source
 
 __kupfer_settings__ = plugin_support.PluginSettings(
     {
         "key": "lang",
-        "label": _("Wikipedia language"),
+        "label": _("Wikipedia languages (separated by ';')"),
         "type": str,
         # TRANS: Default wikipedia language code
         "value": _("en"),
@@ -25,13 +25,55 @@ __kupfer_settings__ = plugin_support.PluginSettings(
 )
 
 
+class Lang(Leaf):
+    def __init__(self, lang):
+        super().__init__(lang, name=f"{lang}.wikipedia.org")
+
+    def get_icon_name(self):
+        return self.object
+
+
+class LangSource(Source):
+    def __init__(self, languages):
+        super().__init__("Languages")
+        self.languages = languages
+
+    def get_items(self):
+        return map(Lang, self.languages)
+
+
 class WikipediaSearch(Action):
     def __init__(self):
         Action.__init__(self, _("Search in Wikipedia"))
+        self._update_settings()
+
+    def initialize(self):
+        __kupfer_settings__.connect_settings_changed_cb(self._update_settings)
+        self._update_settings()
+
+    def _update_settings(self, *_args):
+        languages: tuple[str, ...] = tuple(
+            filter(
+                None,
+                (
+                    lang.strip()
+                    for lang in __kupfer_settings__["lang"].split(";")
+                ),
+            )
+        )
+
+        if not languages:
+            languages = (_("en"),)
+
+        self._languages = languages
 
     def activate(self, leaf, iobj=None, ctx=None):
         # Send in UTF-8 encoding
-        lang_code = __kupfer_settings__["lang"]
+        if iobj:
+            lang_code = iobj.object
+        else:
+            lang_code = self._languages[0]
+
         search_url = f"https://{lang_code}.wikipedia.org/w/index.php?title=Special:Search&go=Go&"
         # will encode search=text, where `text` is escaped
         query_url = search_url + urllib.parse.urlencode({"search": leaf.object})
@@ -41,8 +83,20 @@ class WikipediaSearch(Action):
         yield TextLeaf
 
     def get_description(self):
-        lang_code = __kupfer_settings__["lang"]
-        return _("Search for this term in %s.wikipedia.org") % lang_code
+        if len(self._languages) == 1:
+            lang_code = self._languages[0]
+            return _("Search for this term in %s.wikipedia.org") % lang_code
+
+        return _("Search for this term in %s.wikipedia.org") % ""
 
     def get_icon_name(self):
         return "edit-find"
+
+    def requires_object(self):
+        return len(self._languages) > 1
+
+    def object_source(self, for_item=None):
+        return LangSource(self._languages)
+
+    def object_types(self):
+        yield Lang
